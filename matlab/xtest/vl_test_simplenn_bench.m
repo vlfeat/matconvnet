cc15852ee49238a486c76b208ec47cb3e7360505
Andrea Vedaldi <vedaldi@gmail.com>
2014-04-28 16:20:00 +0100

    restores benchmark

--- matlab/xtest/vl_test_simplenn_bench.m
+++ matlab/xtest/vl_test_simplenn_bench.m
@@ -1,20 +1,20 @@
 n = 3 ;
 T = 40 ;
 
-net = load(fullfile(vlg_root, 'data', 'tinynet_caffe.mat')) ;
-load(fullfile(vlg_root, 'data', 'tinynet_caffe_data.mat')) ;
-dzdy = zeros(1000,1,'single') ;
+net = load(fullfile(vl_rootnn, 'data', 'tinynet_caffe.mat')) ;
+load(fullfile(vl_rootnn, 'data', 'tinynet_caffe_data.mat')) ;
+dzdy = zeros(1,1,1000,'single') ;
 dzdy(286) = 1 ;
 im = repmat(im, [1 1 1 n]) ;
 dzdy = repmat(dzdy, [1 n]) ;
 
 reset(gpuDevice) ;
-net_ = tinynet_move(net, 'gpu') ;
+net_ = vl_simplenn_move(net, 'gpu') ;
 im_ = gpuArray(im) ;
 dzdy_ = gpuArray(dzdy) ;
 
-res_ = tinynet(net_, im_) ;
-res = tinynet(net, im) ;
+res_ = vl_simplenn(net_, im_) ;
+res = vl_simplenn(net, im) ;
 wait(gpuDevice) ;
 
 cpu_times = zeros(T, numel(res)) ;
@@ -23,7 +23,7 @@ gpu_times = zeros(T, numel(res)) ;
 gpu_time = tic ;
 for i=1:T
   %res_ = tinynet(net_, im_, dzdy_) ;
-  res_ = tinynet(net_, im_) ;
+  res_ = vl_simplenn(net_, im_) ;
   gpu_times(i,:) = [res_.time] + [res_.backwardTime] ;
 end
 wait(gpuDevice) ;
@@ -32,7 +32,7 @@ gpu_time = toc(gpu_time) ;
 cpu_time = tic ;
 for i=1:T
   %res = tinynet(net, im, dzdy) ;
-  res = tinynet(net, im) ;
+  res = vl_simplenn(net, im) ;
   cpu_times(i,:) = [res.time] + [res.backwardTime] ;
 end
 cpu_time = toc(cpu_time) ;
