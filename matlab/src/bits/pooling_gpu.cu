41807dd6bba3ebdf6093f81524905a6344e499b5
Karel Lenc <karel@lenc.info>
2014-07-11 15:10:09 +0100

    Fixed the GPU code so it is compilable.

--- matlab/src/bits/pooling_gpu.cu
+++ matlab/src/bits/pooling_gpu.cu
@@ -63,69 +63,137 @@ __global__ void maxPooling_gpu_kernel
   }
 }
 
+
 template<typename T>
-void maxPooling_gpu(T* pooled,
-                    T const* data,
-                    size_t width,
-                    size_t height,
-                    size_t depth,
-                    size_t windowWidth,
-                    size_t windowHeight,
-                    size_t strideX,
-                    size_t strideY,
-                    size_t padLeft,
-                    size_t padRight,
-                    size_t padTop,
-                    size_t padBottom)
+__global__ void avgPooling_gpu_kernel
+(T* pooled,
+ const T* data,
+ const int pooledWidth,
+ const int pooledHeight,
+ const int pooledVolume,
+ const int width,
+ const int height,
+ const int windowWidth,
+ const int windowHeight,
+ const int strideX,
+ const int strideY,
+ const int padLeft,
+ const int padTop)
+{
+  int pooledIndex = threadIdx.x + blockIdx.x * blockDim.x;
+  if (pooledIndex < pooledVolume) {
+    /* pooledIndex = x
+                   + y * pooledWidth
+                   + z * (pooledWidth * pooledHeight) ;
+     */
+    int x = pooledIndex ;
+    int y = x / pooledWidth ;
+    int z = y / pooledHeight ;
+    x %= pooledWidth ;
+    y %= pooledHeight ;
+    int x1 = max(x * strideX - padLeft, 0) ;
+    int y1 = max(y * strideY - padTop, 0) ;
+    int x2 = min(x1 + windowWidth, width) ;
+    int y2 = min(y1 + windowHeight, height) ;
+    data += z * (width*height) ;
+    T accum = 0;
+    T poolSize = (y2 - y1)*(x2 - x1);
+    for (int v = y1 ; v < y2 ; ++v) {
+      for (int u = x1 ; u < x2 ; ++u) {
+        accum += data[v * width + u] ;
+      }
+    }
+    pooled[pooledIndex] = accum / poolSize ;
+  }
+}
+
+
+template<typename T>
+void pooling_gpu(T* pooled,
+                 T const* data,
+                 PoolMethod method,
+                 size_t width,
+                 size_t height,
+                 size_t depth,
+                 size_t windowWidth,
+                 size_t windowHeight,
+                 size_t strideX,
+                 size_t strideY,
+                 size_t padLeft,
+                 size_t padRight,
+                 size_t padTop,
+                 size_t padBottom)
 {
   int pooledWidth = (width + (padLeft+padRight) - windowWidth)/strideX + 1 ;
   int pooledHeight = (height + (padTop+padBottom) - windowHeight)/strideY + 1 ;
   int pooledVolume = pooledWidth * pooledHeight * depth ;
-  maxPooling_gpu_kernel<T>
-  <<< divideUpwards(pooledVolume, VL_CUDA_NUM_THREADS), VL_CUDA_NUM_THREADS >>>
-  (pooled, data,
-   pooledWidth, pooledHeight, pooledVolume,
-   width, height,
-   windowWidth, windowHeight,
-   strideX, strideY,
-   padLeft, padTop);
-  if (cudaGetLastError() != cudaSuccess) {
-    std::cout
-    <<"maxPooling_gpu_kernel error ("
-    <<cudaGetErrorString(cudaGetLastError())
-    <<")"<<std::endl ;
+  switch (method) {
+    case NN_POOL_MAX :
+      maxPooling_gpu_kernel<T>
+      <<< divideUpwards(pooledVolume, VL_CUDA_NUM_THREADS), VL_CUDA_NUM_THREADS >>>
+      (pooled, data,
+       pooledWidth, pooledHeight, pooledVolume,
+       width, height,
+       windowWidth, windowHeight,
+       strideX, strideY,
+       padLeft, padTop);
+      if (cudaGetLastError() != cudaSuccess) {
+        std::cout
+        <<"maxPooling_gpu_kernel error ("
+        <<cudaGetErrorString(cudaGetLastError())
+        <<")"<<std::endl ;
+      }
+      break;
+  case NN_POOL_AVG :
+    avgPooling_gpu_kernel<T>
+    <<< divideUpwards(pooledVolume, VL_CUDA_NUM_THREADS), VL_CUDA_NUM_THREADS >>>
+    (pooled, data,
+     pooledWidth, pooledHeight, pooledVolume,
+     width, height,
+     windowWidth, windowHeight,
+     strideX, strideY,
+     padLeft, padTop);
+    if (cudaGetLastError() != cudaSuccess) {
+      std::cout
+      <<"avgPooling_gpu_kernel error ("
+      <<cudaGetErrorString(cudaGetLastError())
+      <<")"<<std::endl ;
+    }
+    break;
   }
 }
 
 template
-void maxPooling_gpu<float>(float* pooled,
-                           float const* data,
-                           size_t width,
-                           size_t height,
-                           size_t depth,
-                           size_t windowWidth,
-                           size_t windowHeight,
-                           size_t strideX,
-                           size_t strideY,
-                           size_t padLeft,
-                           size_t padRight,
-                           size_t padTop,
-                           size_t padBottom) ;
+void pooling_gpu<float>(float* pooled,
+                        float const* data,
+                        PoolMethod method,
+                        size_t width,
+                        size_t height,
+                        size_t depth,
+                        size_t windowWidth,
+                        size_t windowHeight,
+                        size_t strideX,
+                        size_t strideY,
+                        size_t padLeft,
+                        size_t padRight,
+                        size_t padTop,
+                        size_t padBottom) ;
 
 template
-void maxPooling_gpu<double>(double* pooled,
-                            double const* data,
-                            size_t width,
-                            size_t height,
-                            size_t depth,
-                            size_t windowWidth,
-                            size_t windowHeight,
-                            size_t strideX,
-                            size_t strideY,
-                            size_t padLeft,
-                            size_t padRight,
-                            size_t padTop,
-                            size_t padBottom) ;
+void pooling_gpu<double>(double* pooled,
+                         double const* data,
+                         PoolMethod method,
+                         size_t width,
+                         size_t height,
+                         size_t depth,
+                         size_t windowWidth,
+                         size_t windowHeight,
+                         size_t strideX,
+                         size_t strideY,
+                         size_t padLeft,
+                         size_t padRight,
+                         size_t padTop,
+                         size_t padBottom) ;
 
 /* ---------------------------------------------------------------- */
 /*                                         maxPoolingBackward (GPU) */
@@ -183,64 +251,109 @@ __global__ void maxPoolingBackward_gpu_kernel
      to bottom_diff.
      Caffe goes the other way around, but requrires remembering the layer
      output, or the maximal indexes.
+     atomicAdd(add, val)
      */
     atomicAdd(dzdx + bestIndex, dzdy[pooledIndex]) ;
   }
 }
 
+template<typename T>
+__global__ void avgPoolingBackward_gpu_kernel
+(T* dzdx,
+ const T* dzdy,
+ const int pooledWidth,
+ const int pooledHeight,
+ const int pooledVolume,
+ const int width,
+ const int height,
+ const int windowWidth,
+ const int windowHeight,
+ const int strideX,
+ const int strideY,
+ const int padLeft,
+ const int padTop)
+{
+  // TODO non trivial...
+}
+
+
 
 template<typename T>
-void maxPoolingBackward_gpu(T* dzdx,
-                            T const* data,
-                            T const* dzdy,
-                            PoolMethod method,
-                            size_t width,
-                            size_t height,
-                            size_t depth,
-                            size_t windowWidth,
-                            size_t windowHeight,
-                            size_t strideX,
-                            size_t strideY,
-                            size_t padLeft,
-                            size_t padRight,
-                            size_t padTop,
-                            size_t padBottom)
+void poolingBackward_gpu(T* dzdx,
+                         T const* data,
+                         T const* dzdy,
+                         PoolMethod method,
+                         size_t width,
+                         size_t height,
+                         size_t depth,
+                         size_t windowWidth,
+                         size_t windowHeight,
+                         size_t strideX,
+                         size_t strideY,
+                         size_t padLeft,
+                         size_t padRight,
+                         size_t padTop,
+                         size_t padBottom)
 {
   int pooledWidth = (width + (padLeft+padRight) - windowWidth)/strideX + 1 ;
   int pooledHeight = (height + (padTop+padBottom) - windowHeight)/strideY + 1 ;
   int pooledVolume = pooledWidth * pooledHeight * depth ;
-  maxPoolingBackward_gpu_kernel<T>
-  <<< divideUpwards(pooledVolume, VL_CUDA_NUM_THREADS), VL_CUDA_NUM_THREADS >>>
-  (dzdx,
-   data, dzdy,
-   pooledWidth, pooledHeight, pooledVolume,
-   width, height,
-   windowWidth, windowHeight,
-   strideX, strideY,
-   padLeft, padTop);
-  if (cudaGetLastError() != cudaSuccess) {
-    std::cout
-    <<"maxPooling_gpu_kernel error ("
-    <<cudaGetErrorString(cudaGetLastError())
-    <<")"<<std::endl ;
+  switch (method) {
+    case NN_POOL_MAX:
+      maxPoolingBackward_gpu_kernel<T>
+      <<< divideUpwards(pooledVolume, VL_CUDA_NUM_THREADS), VL_CUDA_NUM_THREADS >>>
+      (dzdx,
+       data, dzdy,
+       pooledWidth, pooledHeight, pooledVolume,
+       width, height,
+       windowWidth, windowHeight,
+       strideX, strideY,
+       padLeft, padTop);
+      if (cudaGetLastError() != cudaSuccess) {
+        std::cout
+        <<"maxPooling_gpu_kernel error ("
+        <<cudaGetErrorString(cudaGetLastError())
+        <<")"<<std::endl ;
+      }
+      break;
+    case NN_POOL_AVG:
+      avgPoolingBackward_gpu_kernel<T>
+      <<< divideUpwards(pooledVolume, VL_CUDA_NUM_THREADS), VL_CUDA_NUM_THREADS >>>
+      (dzdx,
+       dzdy,
+       pooledWidth, pooledHeight, pooledVolume,
+       width, height,
+       windowWidth, windowHeight,
+       strideX, strideY,
+       padLeft, padTop);
+      if (cudaGetLastError() != cudaSuccess) {
+        std::cout
+        <<"avgPooling_gpu_kernel error ("
+        <<cudaGetErrorString(cudaGetLastError())
+        <<")"<<std::endl ;
+      }
+      break;
   }
+
+
 }
 
 template
-void maxPoolingBackward_gpu<float>(float* dzdx,
-                                   float const* data,
-                                   float const* dzdy,
-                                   size_t width,
-                                   size_t height,
-                                   size_t depth,
-                                   size_t windowWidth,
-                                   size_t windowHeight,
-                                   size_t strideX,
-                                   size_t strideY,
-                                   size_t padLeft,
-                                   size_t padRight,
-                                   size_t padTop,
-                                   size_t padBottom) ;
+void poolingBackward_gpu<float>(float* dzdx,
+                                float const* data,
+                                float const* dzdy,
+                                PoolMethod method,
+                                size_t width,
+                                size_t height,
+                                size_t depth,
+                                size_t windowWidth,
+                                size_t windowHeight,
+                                size_t strideX,
+                                size_t strideY,
+                                size_t padLeft,
+                                size_t padRight,
+                                size_t padTop,
+                                size_t padBottom) ;
 
 #if 0
 template
