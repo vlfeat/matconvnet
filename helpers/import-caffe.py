a3fe38ba4575f0da0e02319cac889eb03035bdf1
Karel Lenc <karel@robots.ox.ac.uk>
2014-07-25 21:29:57 +0100

    Several bugfixes in python export script.
    Adjusted output data format,
    parameters conversion for the first fully connected layer
     (needs testing),
    additional padding for even-sized data pooling
     (assumes that only odd-sized pooling kernel makes sense)

--- helpers/import-caffe.py
+++ helpers/import-caffe.py
@@ -5,6 +5,7 @@ import vgg_caffe_pb2 as caffe_pb2
 import google.protobuf
 import numpy as np
 import scipy.io as sio
+from math import floor, ceil
 from numpy import array
 
 def blobproto_to_array(blob, return_diff=False):
@@ -20,16 +21,27 @@ def blobproto_to_array(blob, return_diff=False):
 
 param_file='../data/vgg/CNN_F/param.prototxt'
 model_file='../data/vgg/CNN_F/model'
+out_file='../data/cnn_f.mat'
+
+pool_methods = ['max', 'avg'];
 
 # load network parameters
+print('Loading caffe-def {}'.format(param_file))
 net=caffe_pb2.NetParameter()
 with open(param_file, 'r') as f:
   google.protobuf.text_format.Merge(f.read(), net)
+print('Loading caffe-data {}'.format(model_file))
 net_bin=caffe_pb2.NetParameter()
 with open(model_file, 'rb') as f:
   net_bin.MergeFromString(f.read())
 
+# TODO set layer sizes when blobs not provided
+# TODO add command line options (netdef, netdata, schemefile)
+
+print('Converting {} layers'.format(len(net.layers)))
 matlabLayers = []
+prev_out_sz = [net.input_dim[2], net.input_dim[3], net.input_dim[1]]
+print('Input size: {} {} {}'.format(prev_out_sz[0], prev_out_sz[1], prev_out_sz[2]))
 layers = [x.layer.name for x in net.layers]
 layers_bin = [x.layer.name for x in net_bin.layers]
 print layers
@@ -63,10 +75,15 @@ for name in layers:
       else:
         mk['biases'] = np.zeros([0,0],dtype='float32')
       if hasattr(layer, 'pad'):
-        mk['pad'] = layer.pad
+        mk['pad'] = float(layer.pad)
       else:
-        mk['pad'] = 0
-      mk['stride'] = layer.stride
+        mk['pad'] = 0.
+      mk['stride'] = float(layer.stride)
+      prev_out_sz = [\
+        floor((prev_out_sz[0] - mk['filters'].shape[0]) / layer.stride) + 1 + 2*mk['pad'], \
+        floor((prev_out_sz[1] - mk['filters'].shape[1]) / layer.stride) + 1 + 2*mk['pad'], \
+        mk['filters'].shape[3]]
+      print('New size: {} {} {}'.format(prev_out_sz[0], prev_out_sz[1], prev_out_sz[2]))
     elif layer.type == 'pad':
       mk['type'] = 'pad'
       pass
@@ -77,28 +94,43 @@ for name in layers:
       mk['param'] = np.array([layer.local_size, layer.k, layer.alpha, layer.beta])
     elif layer.type == 'pool':
       mk['type'] = 'pool'
-      mk['pool'] = layer.kernelsize
-      mk['method'] = layer.pool
+      mk['pool'] = float(layer.kernelsize)
+      mk['method'] = pool_methods[layer.pool]
+      pad = 0
       if hasattr(layer, 'pad'):
-        mk['pad'] = layer.pad
+        mk['pad'] = float(layer.pad)*np.array([1., 1., 1., 1.])
+        pad = layer.pad
       else:
-        mk['pad'] = 0
-      mk['stride'] = layer.stride
+        mk['pad'] = np.array([0., 0., 0., 0.])
+      # Add single pixel right/bottom padding for even sized inputs
+      if prev_out_sz[0] % 2 == 0:
+        mk['pad'][1] += 1
+      if prev_out_sz[1] % 2 == 0:
+        mk['pad'][3] += 1
+      mk['stride'] = float(layer.stride)
+      prev_out_sz = [\
+        ceil((prev_out_sz[0] - mk['pool']) / layer.stride) + 1 + 2*pad, \
+        ceil((prev_out_sz[1] - mk['pool']) / layer.stride) + 1 + 2*pad, \
+        prev_out_sz[2]]
+      print('New size: {} {} {}'.format(prev_out_sz[0], prev_out_sz[1], prev_out_sz[2]))
     elif layer.type == 'innerproduct':
       mk['type'] = 'conv'
       if len(arrays) >= 1:
-        mk['filters'] = arrays[0].transpose([0, 1, 3, 2])
+        mk['filters'] = arrays[0].reshape((prev_out_sz[1], prev_out_sz[0], prev_out_sz[2], -1))
+        mk['filters'].transpose([1, 0, 2, 3])
       else:
         mk['filters'] = np.zeros([0,0],dtype='float32')
       if len(arrays) >= 2:
         mk['biases'] = np.squeeze(arrays[1].transpose([2, 3, 1, 0]), (2,3))
       else:
         mk['biases'] = np.zeros([0,0],dtype='float32')
-      mk['pad'] = 0
-      mk['stride'] = 1
+      mk['pad'] = 0.
+      mk['stride'] = 1.
+      prev_out_sz = [ 1, 1, mk['filters'].shape[3]];
+      print('New size: {} {} {}'.format(prev_out_sz[0], prev_out_sz[1], prev_out_sz[2]))
     elif layer.type == 'dropout':
       mk['type'] = 'dropout'
-      mk['rate']= layer.dropout_ratio
+      mk['rate']= float(layer.dropout_ratio)
     elif layer.type == 'softmax':
       mk['type'] = 'softmax'
     else:
@@ -117,6 +149,7 @@ else:
   mkNormalization['imageSize']=np.array([0,0],dtype='float32')
 mkNormalization['averageImage']=np.array([0,0],dtype='float32')
 
-sio.savemat('test.mat', {\
+print('Exporting to {}'.format(out_file))
+sio.savemat(out_file, {\
 'layers':np.array(matlabLayers),\
 'normalization': mkNormalization})
