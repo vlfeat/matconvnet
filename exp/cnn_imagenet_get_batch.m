ce062e4e82b9de27272ad94fad698fb8eb69497d
Andrea Vedaldi <vedaldi@gmail.com>
2014-08-05 18:43:31 +0200

    reversed `fix’: fetch != prefetch and indicates that images are already loaded into memory and passed in as input argument this is essential to use cnn_imagenet_get_batch with images as input (instead of filenames)

--- exp/cnn_imagenet_get_batch.m
+++ exp/cnn_imagenet_get_batch.m
@@ -34,7 +34,7 @@ if opts.numThreads > 0
   end
 end
 if ~fetch
-  im = cell(1, numel(images)) ;
+  im = images ;
 end
 
 imo = zeros(opts.size(1), opts.size(2), 3, numel(images)*opts.numAugments, 'single') ;
