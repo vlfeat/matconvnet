482ff09437d3a6cead1e3d1e6c082bdba5d689a7
Andrea Vedaldi <vedaldi@gmail.com>
2014-08-13 12:24:36 +0100

    cnn_imagenet_minimal.m: update to a simpler example

--- examples/cnn_imagenet_minimal.m
+++ examples/cnn_imagenet_minimal.m
@@ -1,29 +1,29 @@
 function cnn_imagenet_minimal()
-% CNN_IMAGENET_MINIMAL   Minimalistic demonstration of how to run a CNN model
+% CNN_IMAGENET_MINIMAL   Minimalistic demonstration of how to run an ImageNet CNN model
 
 % setup toolbox
 run(fullfile(fileparts(mfilename('fullpath')), '../matlab/vl_setupnn.m')) ;
 
-% load a CNN
-net = load('data/models/imagenet-caffe-ref.mat') ;
+% download a pre-trained CNN from the web
+if ~exist('imagenet-vgg-f.mat')
+  urlwrite('http://www.vlfeat.org/matconvnet/models/imagenet-vgg-f.mat', ...
+    'imagenet-vgg-f.mat') ;
+end
+net = load('imagenet-vgg-f.mat') ;
 
-% load an image
-im = imread('http://www.vlfeat.org/sandbox-matconvnet/caffe-cat.jpg') ;
-
-% preprocess the image (see also CNN_IMAGENET_GET_BATCH)
+% obtain and preprocess an image
+im = imread('peppers.png') ;
 im_ = single(im) ; % note: 255 range
-im_ = imresize(im_, ...
-  net.normalization.imageSize(1:2), ...
-  net.normalization.interpolation) ;
+im_ = imresize(im_, net.normalization.imageSize(1:2)) ;
 im_ = im_ - net.normalization.averageImage ;
 
-% evaluate the CNN
+% run the CNN
 res = vl_simplenn(net, im_) ;
 
-% show result
+% show the classification result
 scores = squeeze(gather(res(end).x)) ;
 [bestScore, best] = max(scores) ;
-figure(1) ; clf ;
-imagesc(im) ; title(sprintf('%s (%d), score %.3f',...
-  net.classes.description{best}, best, bestScore)) ;
-axis off
+figure(1) ; clf ; imagesc(im) ;
+title(sprintf('%s (%d), score %.3f',...
+   net.classes.description{best}, best, bestScore)) ;
+
