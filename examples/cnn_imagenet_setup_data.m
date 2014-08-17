c0f04c59322149fee01ef54e613f2fd75de980bf
Andrea Vedaldi <vedaldi@gmail.com>
2014-08-17 12:09:48 +0100

    imagenet: improves handling of ImageNet package

--- examples/cnn_imagenet_setup_data.m
+++ examples/cnn_imagenet_setup_data.m
@@ -1,13 +1,38 @@
 function imdb = cnn_imagenet_setup_data(varargin)
-% CNN_IMAGENET_SETPU_DATA  Initialize ImageNet data
-%    This function creates an image database IMDB pointing to the
-%    ImageNet12 data. This data should be already contained on disk. The
-%    IMDB just list the images in the dataset, but does not contain the
-%    actual image data.
+% CNN_IMAGENET_SETUP_DATA  Initialize ImageNet ILSVRC CLS-LOC challenge data
+%    This function creates an IMDB structure pointing to a local copy
+%    of the ILSVRC CLS-LOC data.
 %
-%    Note that, in order to speedup training and testing, it may be a good
-%    idea to preprocess the images to have a fixed size (e.g. 256 pixels
-%    height) and to store the resulting images on RAM disk (provided that
+%    The ILSVRC data ships in several TAR archives that can be
+%    obtained from the ILSVRC challenge website. You will need:
+%
+%    ILSVRC2012_img_train.tar
+%    ILSVRC2012_img_val.tar
+%    ILSVRC2012_img_test.tar
+%    ILSVRC2012_devkit.tar
+%
+%    Note that images in the CLS-LOC challenge are the same for the 2012, 2013,
+%    and 2014 edition of ILSVRC, but that the development kit
+%    is different. However, all devkit versions should work.
+%
+%    In order to use the ILSVRC data with these scripts, please
+%    unpack it as follows. Create a root folder <DATA>, by default
+%
+%    data/imagenet12
+%
+%    (note that this can be a simlink). Use the 'dataDir' option to
+%    specify a different path.
+%
+%    Within this folder, create the following hierarchy:
+%
+%    <DATA>/images/train/ : content of ILSVRC2012_img_train.tar
+%    <DATA>/images/val/ : content of ILSVRC2012_img_val.tar
+%    <DATA>/images/test/ : content of ILSVRC2012_img_test.tar
+%    <DATA>/ILSVRC2012_devkit : content of ILSVRC2012_devkit.tar
+%
+%    In order to speedup training and testing, it may be a good idea
+%    to preprocess the images to have a fixed size (e.g. 256 pixels
+%    high) and/or to store the images in RAM disk (provided that
 %    sufficient RAM is available). Reading images off disk with a
 %    sufficient speed is crucial for fast training.
 
@@ -15,55 +40,129 @@ opts.dataDir = 'data/imagenet12' ;
 opts.lite = false ;
 opts = vl_argparse(opts, varargin) ;
 
-metaPath = fullfile(opts.dataDir, 'ILSVRC2012_devkit_t12', 'data', 'meta.mat') ;
-valLabelsPath = fullfile(opts.dataDir, 'ILSVRC2012_devkit_t12', 'data', 'ILSVRC2012_validation_ground_truth.txt') ;
-trainImageListPath = fullfile(opts.dataDir, 'imagesets', 'train.txt') ;
-valImageListPath = fullfile(opts.dataDir, 'imagesets', 'val.txt') ;
-testImageListPath = fullfile(opts.dataDir, 'imagesets', 'test.txt') ;
+% -------------------------------------------------------------------------
+%                                                  Load categories metadata
+% -------------------------------------------------------------------------
 
-if ~exist(metaPath)
-  error('Make sure that ImageNet is installed in %s (%s not found)', ...
+d = dir(fullfile(opts.dataDir, '*devkit*')) ;
+if numel(d) == 0
+  error('Make sure that ILSVRC data is installed in %s (%s not found)', ...
     opts.dataDir, metaPath) ;
 end
+devkitPath = fullfile(opts.dataDir, d(1).name) ;
 
-% load categories metadata
+% find metadata
+mt = dir(fullfile(devkitPath, 'data', 'meta_clsloc.mat')) ;
+if numel(mt) == 0
+  mt = dir(fullfile(devkitPath, 'data', 'meta.mat')) ;
+end
+metaPath = fullfile(devkitPath, 'data', mt(1).name) ;
+
+% find validation images labels
+tmp = dir(fullfile(devkitPath, 'data', '*_validation_ground_truth*')) ;
+valLabelsPath = fullfile(devkitPath, 'data', tmp(1).name) ;
+
+% find validation images blacklist
+tmp = dir(fullfile(devkitPath, 'data', '*_validation_blacklist*')) ;
+if numel(tmp) > 0
+  valBlacklistPath = fullfile(devkitPath, 'data', tmp(1).name) ;
+else
+  valBlacklistPath = [] ;
+  warning('Could not find validation images blacklist file');
+end
+
+fprintf('using devkit %s\n', devkitPath) ;
+fprintf('using metadata %s\n', metaPath) ;
+fprintf('using validation labels %s\n', valLabelsPath) ;
+fprintf('using validation blacklist %s\n', valBlacklistPath) ;
+
 meta = load(metaPath) ;
 cats = {meta.synsets(1:1000).WNID} ;
 descrs = {meta.synsets(1:1000).words} ;
 
-% load list of training images
-names = textread(trainImageListPath, '%s')' ;
-imageCats = regexp(names, '^[^/]+', 'match', 'once') ;
-[~,labels] = ismember(imageCats, cats) ;
+imdb.classes.name = cats ;
+imdb.classes.description = descrs ;
+imdb.imageDir = fullfile(opts.dataDir, 'images') ;
+
+% -------------------------------------------------------------------------
+%                                                           Training images
+% -------------------------------------------------------------------------
+
+fprintf('searching training images ...\n') ;
+names = {} ;
+labels = {} ;
+for d = dir(fullfile(opts.dataDir, 'images', 'train', 'n*'))'
+  [~,lab] = ismember(d.name, cats) ;
+  ims = dir(fullfile(opts.dataDir, 'images', 'train', d.name, '*.JPEG')) ;
+  names{end+1} = strcat([d.name, filesep], {ims.name}) ;
+  labels{end+1} = ones(1, numel(ims)) * lab ;
+  fprintf('.') ;
+  if mod(numel(names), 50) == 0, fprintf('\n') ; end
+  %fprintf('found %s with %d images\n', d.name, numel(ims)) ;
+end
+names = horzcat(names{:}) ;
+labels = horzcat(labels{:}) ;
+
+if numel(names) ~= 1281167
+  warning('Found %d training images instead of 1281167', numel(names)) ;
+end
+
+names = strcat(['train' filesep], names) ;
 
 imdb.images.id = 1:numel(names) ;
 imdb.images.name = names ;
 imdb.images.set = ones(1, numel(names)) ;
 imdb.images.label = labels ;
-imdb.classes.name = cats ;
-imdb.classes.description = descrs ;
 
-% load list of validation images
-names = sort(textread(valImageListPath, '%s')') ;
-labels = textread(valLabelsPath, '%d')' ;
+% -------------------------------------------------------------------------
+%                                                         Validation images
+% -------------------------------------------------------------------------
+
+ims = dir(fullfile(opts.dataDir, 'images', 'val', '*.JPEG')) ;
+names = sort({ims.name}) ;
+labels = textread(valLabelsPath, '%d') ;
+
+if numel(labels) ~= 50e3
+  warning('Found %d instead of 50000 validation images', numel(labels))
+end
+
+if ~isempty(valBlacklistPath)
+  black = textread(valBlacklistPath, '%d') ;
+  fprintf('blacklisting %d validation images\n', numel(black)) ;
+  keep = setdiff(1:numel(names), black) ;
+  names = names(keep) ;
+  labels = labels(keep) ;
+end
+
+names = strcat(['val' filesep], names) ;
 
 imdb.images.id = horzcat(imdb.images.id, (1:numel(names)) + 1e7 - 1) ;
 imdb.images.name = horzcat(imdb.images.name, names) ;
 imdb.images.set = horzcat(imdb.images.set, 2*ones(1,numel(names))) ;
-imdb.images.label = horzcat(imdb.images.label, labels) ;
+imdb.images.label = horzcat(imdb.images.label, labels') ;
 
-% load list of test images
-names = textread(testImageListPath, '%s')' ;
+% -------------------------------------------------------------------------
+%                                                               Test images
+% -------------------------------------------------------------------------
+
+ims = dir(fullfile(opts.dataDir, 'images', 'test', '*.JPEG')) ;
+names = sort({ims.name}) ;
 labels = zeros(1, numel(names)) ;
+
+if numel(labels) ~= 100e3
+  warning('Found %d instead of 100000 test images', numel(labels))
+end
+
+names = strcat(['test' filesep], names) ;
 
 imdb.images.id = horzcat(imdb.images.id, (1:numel(names)) + 2e7 - 1) ;
 imdb.images.name = horzcat(imdb.images.name, names) ;
 imdb.images.set = horzcat(imdb.images.set, 3*ones(1,numel(names))) ;
 imdb.images.label = horzcat(imdb.images.label, labels) ;
 
-% fixes fields
-imdb.images.name = strcat(imdb.images.name, '.JPEG') ;
-imdb.imageDir = fullfile(opts.dataDir, 'images') ;
+% -------------------------------------------------------------------------
+%                                                            Postprocessing
+% -------------------------------------------------------------------------
 
 % sort categories by WNID (to be compatible with other implementations)
 [imdb.classes.name,perm] = sort(imdb.classes.name) ;
--- utils/preprocess-imagenet.sh
+++ utils/preprocess-imagenet.sh
@@ -3,32 +3,48 @@
 # auhtor: Andrea Vedaldi
 
 # Use as:
-#   preprocess-imagenet.sh ILSVRC2012_PATH DEST_PATH
+#   preprocess-imagenet.sh SRC_PATH DEST_PATH
 #
-# This will copy the parts of the ILSVRC2012 data (devkit) and convert
-# images to have a height of 256 pixels.
+# The script creates a copy of the ImageNet ILSVRC CLS-LOC challenge
+# data while rescaling the images. The data is supposed to be in the
+# format defined by examples/cnn_imagenet_setup_data.m
+#
+# Images are rescaled to a height of 256 pixels.
 
 data=$1
 ram=$2
 num_cpus=8
 
-ln -sf $ram ./data/imagenet12-ram
-mkdir -p $ram/{images,imagesets}
-rsync -rv --chmod=ugo=rwX $data/ILSVRC2012_devkit_t12 $ram/
-rsync -rv --chmod=ugo=rwX $data/imagesets/{train,val,test}.txt $ram/imagesets/
+mkdir -p "$ram"/images ;
+rsync -rv --chmod=ugo=rwX "$data"/*devkit* "$ram/"
 
-dirs="$data/images/val1 $data/images/val2 $(ls -d $data/images/n*)"
+function convert_one()
+{
+    out="$1"
+    shift
+    for infile in "$@"
+    do
+        outfile="$out/$(basename $infile)"
+        if test -f "$outfile"
+        then
+            continue ;
+        fi
+        convert -verbose -format jpeg -quality 90 -resize x256 \
+            "$infile" "${outfile}.temp"
+        mv "${outfile}.temp" "$outfile"
+    done
+}
+export -f convert_one
+
+
+dirs=$(find $data/images/* -maxdepth 2 -type d)
 for d in $dirs
 do
-    sub=$(basename $d)
-    out=$ram/images/$sub
-    echo "Converting $d"
+    sub=${d#${data}/images/}
+    out="$ram/images/$sub"
+    echo "Converting $d -> $out"
     mkdir -p "$out"
-    find "$d" -name '*.JPEG' | \
-    while read f; do
-        test -f "$out/$(basename $f)" || echo "$f"
-    done | \
-        xargs -n 10 --max-procs=$num_cpus \
-        convert -verbose -format jpeg -quality 90 -resize x256 \
-        -set filename:f "$out/%f" -write '%[filename:f]'
+    find "$d" -maxdepth 1 -name '*.JPEG' | \
+        xargs -n 100 --max-procs=$num_cpus \
+        bash -c "convert_one \"$out\" \"\$@\"" _
 done
