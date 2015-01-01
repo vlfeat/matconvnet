51d432fe50448b75294ac8657aa4b9df9ac8bc4a
Andrea Vedaldi <vedaldi@gmail.com>
2015-01-01 17:54:34 +0100

    vl_nnloss.m: fixes a typo in the documentation

--- matlab/vl_nnloss.m
+++ matlab/vl_nnloss.m
@@ -4,8 +4,8 @@ function Y = vl_nnloss(X,c,dzdy)
 %    X. X has dimension H x W x D x N, packing N arrays of W x H
 %    D-dimensional vectors.
 %
-%    C contains the class labels, which should be integer in the range
-%    1 to D.  C can be an array with either N elements or with H x W x
+%    C contains the class labels, which should be integers in the range
+%    1 to D. C can be an array with either N elements or with H x W x
 %    1 x N dimensions. In the fist case, a given class label is
 %    applied at all spatial locations; in the second case, different
 %    class labels can be specified for different locations.
