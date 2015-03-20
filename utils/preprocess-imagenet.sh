a8f68bd88875ddca867d0b3d6c33dd8399995110
Andrea Vedaldi <vedaldi@gmail.com>
2015-03-20 17:48:02 +0000

    prerpocess-imagenet.sh: adds support for GraphicsMagick

--- utils/preprocess-imagenet.sh
+++ utils/preprocess-imagenet.sh
@@ -13,13 +13,21 @@
 
 data=$1
 ram=$2
+
+# graphics magick (faster)
+num_cpus=1
+method=gm
+
+# image magick
 num_cpus=8
+method=im
 
 mkdir -p "$ram"/images ;
 rsync -rv --chmod=ugo=rwX "$data"/*devkit* "$ram/"
 
-function convert_some()
+function convert_some_im()
 {
+    gm=gm
     out="$1"
     shift
     for infile in "$@"
@@ -34,7 +42,26 @@ function convert_some()
         mv "${outfile}.temp" "$outfile"
     done
 }
-export -f convert_some
+export convert_some_im
+
+function convert_some_gm()
+{
+    gm=gm
+    out="$1"
+    shift
+    for infile in "$@"
+    do
+        outfile="$out/$(basename $infile)"
+        if test -f "$outfile"
+        then
+            continue ;
+        fi
+        echo convert -verbose "'$infile'" -quality 90 -resize 256x256^ \
+            JPEG:"${outfile}"
+    done | ${gm} batch -echo on -feedback on -
+}
+export -f convert_some_gm
+
 
 dirs=$(find $data/images/* -maxdepth 2 -type d)
 for d in $dirs
@@ -44,6 +71,6 @@ do
     echo "Converting $d -> $out"
     mkdir -p "$out"
     find "$d" -maxdepth 1 -name '*.JPEG' | \
-        xargs -n 100 --max-procs=$num_cpus \
-        bash -c "convert_some \"$out\" \"\$@\"" _
+        xargs -n 1000 --max-procs=$num_cpus \
+        bash -c "convert_some_$method \"$out\" \"\$@\"" _
 done
