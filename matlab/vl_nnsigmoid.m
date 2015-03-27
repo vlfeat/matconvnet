4755f19194f1d57cd5f9e5aa93c65200a48e971a
Andrea Vedaldi <vedaldi@gmail.com>
2015-03-27 10:18:21 +0000

    vl_nnsigmoid: cosmetic fixes.

--- matlab/vl_nnsigmoid.m
+++ matlab/vl_nnsigmoid.m
@@ -1,13 +1,15 @@
 function out = vl_nnsigmoid(x,dzdy)
 % VL_NNSIGMOID  Sigmoid nonlinearity
-%   Y = VL_NNSIGMOID(X) appliessigmoid function to the data X. 
-%   X can have arbitrary size.
+%   Y = VL_NNSIGMOID(X) computes the sigmoid of the data X. X can
+%   have an arbitrary size. The sigmoid is defined as follows:
+%
+%     SIGMOID(X) = 1 / (1 + EXP(-X)).
 %
 %   DZDX = VL_NNSIGMOID(X, DZDY) computes the network derivative DZDX
 %   with respect to the input X given the derivative DZDY with respect
 %   to the output Y. DZDX has the same dimension as X.
 
-% Copyright (C) 2014 Karel Lenc, Andrea Vedaldi.
+% Copyright (C) 2015 Karel Lenc.
 % All rights reserved.
 %
 % This file is part of the VLFeat library and is made available under
