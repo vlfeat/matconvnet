22162ef244eae5b465afff79404b2584028f592d
Andrea Vedaldi <vedaldi@gmail.com>
2015-06-13 21:37:15 +0100

    cnn_imagenet_setup_data.m: fixes lite version

--- examples/cnn_imagenet_setup_data.m
+++ examples/cnn_imagenet_setup_data.m
@@ -178,16 +178,17 @@ imdb.images.label(ok) = relabel(imdb.images.label(ok)) ;
 if opts.lite
   % pick a small number of images for the first 10 classes
   % this cannot be done for test as we do not have test labels
+  clear keep ;
   for i=1:10
     sel = find(imdb.images.label == i) ;
     train = sel(imdb.images.set(sel) == 1) ;
     val = sel(imdb.images.set(sel) == 2) ;
-    train = train(1:10) ;
-    val = val(1:3) ;
+    train = train(1:256) ;
+    val = val(1:40) ;
     keep{i} = [train val] ;
   end
   test = find(imdb.images.set == 3) ;
-  keep = sort(cat(2, keep{:}, test(1:30))) ;
+  keep = sort(cat(2, keep{:}, test(1:1000))) ;
   imdb.images.id = imdb.images.id(keep) ;
   imdb.images.name = imdb.images.name(keep) ;
   imdb.images.set = imdb.images.set(keep) ;
