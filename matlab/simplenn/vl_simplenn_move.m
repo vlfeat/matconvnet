16a8cd62b59b9bd7216abf0881b728a786cdd751
Andrea Vedaldi <vedaldi@gmail.com>
2015-07-21 08:11:40 +0100

    vl_simplenn_move: fixes #206

--- matlab/simplenn/vl_simplenn_move.m
+++ matlab/simplenn/vl_simplenn_move.m
@@ -19,7 +19,7 @@ switch destination
 end
 for l=1:numel(net.layers)
   switch net.layers{l}.type
-    case {'conv', 'bnorm'}
+    case {'conv', 'convt', 'bnorm'}
       for f = {'filters', 'biases', 'filtersMomentum', 'biasesMomentum'}
         f = char(f) ;
         if isfield(net.layers{l}, f)
