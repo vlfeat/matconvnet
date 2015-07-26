2065503e24f92369d9348ef5798ba8bd1a692b91
Andrea Vedaldi <vedaldi@gmail.com>
2015-07-26 12:09:17 +0100

    vl_setpunn.m: removes dead code

--- matlab/vl_setupnn.m
+++ matlab/vl_setupnn.m
@@ -12,5 +12,4 @@ root = vl_rootnn() ;
 addpath(fullfile(root, 'matlab')) ;
 addpath(fullfile(root, 'matlab', 'mex')) ;
 addpath(fullfile(root, 'matlab', 'simplenn')) ;
-%addpath(fullfile(root, 'matlab', 'dagnn')) ;
 addpath(fullfile(root, 'matlab', 'xtest')) ;
