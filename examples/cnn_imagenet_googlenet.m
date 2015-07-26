d0ec4e031e21ba163aafa158d4bcf4409bc69355
Andrea Vedaldi <vedaldi@gmail.com>
2015-07-26 12:23:35 +0100

    dagnn: add a few examples

---
+++ examples/cnn_imagenet_googlenet.m
@@ -0,0 +1,22 @@
+run matlab/vl_setupnn
+netData = load('data/models/imagenet-googlenet-dag.mat') ;
+net = dagnn.DagNN.loadobj(netData) ;
+clear netData ;
+
+net.removeLayer(net.layers(end).name) ;
+net.addLayer('softmax', dagnn.SoftMax(), ...
+             net.layers(end).outputs, {'prediction'}, {}) ;
+net.mode = 'test' ;
+
+im = imread('peppers.png') ;
+im_ = single(im) ; % note: 255 range
+im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
+im_ = im_ - net.meta.normalization.averageImage ;
+net.eval({'data', im_, 'label', ones(1,1,1)}) ;
+
+% show the classification result
+scores = squeeze(gather(net.vars(end).value)) ;
+[bestScore, best] = max(scores) ;
+figure(1) ; clf ; imagesc(im) ;
+title(sprintf('%s (%d), score %.3f',...
+net.meta.description{best}, best, bestScore)) ;
