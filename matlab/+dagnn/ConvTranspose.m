14412381533f9c371a352b749987e75362b3df4f
Andrea Vedaldi <vedaldi@gmail.com>
2015-08-18 18:09:02 +0200

    ConvTranspose.m: bugfix (closes #221)

--- matlab/+dagnn/ConvTranspose.m
+++ matlab/+dagnn/ConvTranspose.m
@@ -7,7 +7,7 @@ classdef ConvTranspose < dagnn.Layer
     numGroups = 1
     opts = {'cuDNN'}
   end
-  
+
   methods
     function outputs = forward(obj, inputs, params)
       if ~obj.hasBias, params{2} = [] ; end
@@ -18,7 +18,7 @@ classdef ConvTranspose < dagnn.Layer
         'numGroups', obj.numGroups, ...
         obj.opts{:}) ;
     end
-    
+
     function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
       if ~obj.hasBias, params{2} = [] ; end
       [derInputs{1}, derParams{1}, derParams{2}] = vl_nnconvt(...
@@ -26,24 +26,24 @@ classdef ConvTranspose < dagnn.Layer
         'upsample', obj.upsample, ...
         'crop', obj.crop, ...
         'numGroups', obj.numGroups, ...
-        obj.opts{:}) ;      
+        obj.opts{:}) ;
     end
-    
+
     function outputSizes = getOutputSizes(obj, inputSizes)
       outputSizes{1} = [...
-        obj.upsalmple(1) * (inputSizes{1}(1) - 1) + obj.size(1) - obj.crop(1) - obj.crop(2), ...
-        obj.upsalmple(2) * (inputSizes{1}(2) - 1) + obj.size(2) - obj.crop(3) - obj.crop(4), ...
+        obj.upsample(1) * (inputSizes{1}(1) - 1) + obj.size(1) - obj.crop(1) - obj.crop(2), ...
+        obj.upsample(2) * (inputSizes{1}(2) - 1) + obj.size(2) - obj.crop(3) - obj.crop(4), ...
         obj.size(4), ...
         inputSizes{1}(4)] ;
     end
-    
+
     function params = initParams(obj)
       % todo: test this initialization method
       sc = sqrt(2 / prod(obj.size([1 2 4]))) ;
       params{1} = randn(obj.size,'single') * sc ;
       params{2} = zeros(obj.size(3),1,'single') * sc ;
     end
-    
+
     function obj = ConvTranspose(varargin)
       obj.load(varargin) ;
     end
