20717fe6b812c26475b6b503a01925260d8a5f40
Andrea Vedaldi <vedaldi@gmail.com>
2015-10-13 18:49:13 +0100

    cnn_train_dag.m: better naming of losses

--- examples/cnn_train_dag.m
+++ examples/cnn_train_dag.m
@@ -301,7 +301,7 @@ function stats = extractStats(net)
 sel = find(cellfun(@(x) isa(x,'dagnn.Loss'), {net.layers.block})) ;
 stats = struct() ;
 for i = 1:numel(sel)
-  stats.(net.layers(sel(i)).name) = net.layers(sel(i)).block.average ;
+  stats.(net.layers(sel(i)).outputs{1}) = net.layers(sel(i)).block.average ;
 end
 
 % -------------------------------------------------------------------------
