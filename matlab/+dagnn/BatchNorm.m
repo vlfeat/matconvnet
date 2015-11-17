19fd307f92d389eb72e3210061efbb14405bbca5
Andrea Vedaldi <vedaldi@gmail.com>
2015-11-17 16:33:55 +0000

    DagNN: adds support for training moments in batch normalization

--- matlab/+dagnn/BatchNorm.m
+++ matlab/+dagnn/BatchNorm.m
@@ -1,13 +1,27 @@
 classdef BatchNorm < dagnn.ElementWise
   methods
     function outputs = forward(obj, inputs, params)
-      outputs{1} = vl_nnbnorm(inputs{1}, params{1}, params{2}) ;
+      if strcmp(obj.net.mode, 'test')
+        outputs{1} = vl_nnbnorm(inputs{1}, params{1}, params{2}, 'moments', params{3}) ;
+      else
+        outputs{1} = vl_nnbnorm(inputs{1}, params{1}, params{2}) ;
+      end
     end
 
     function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
-      derInputs{1} = vl_nnrelu(inputs{1}, derOutputs{1}) ;
-      [derInputs{1}, derParams{1}, derParams{2}] = ...
-          vl_nnbnorm(inputs{1}, params{1}, params{2}, derOutputs{1}) ;
+      [derInputs{1}, derParams{1}, derParams{2}, derParams{3}] = ...
+        vl_nnbnorm(inputs{1}, params{1}, params{2}, derOutputs{1}) ;
+    end
+
+    % ---------------------------------------------------------------------
+    function obj = BatchNorm(varargin)
+      obj.load(varargin{:}) ;
+    end
+
+    function attach(obj, net, index)
+      attach@dagnn.ElementWise(obj, net, index) ;
+      p = net.getParamIndex(net.layers(index).params{3}) ;
+      net.params(p).trainMethod = 'average' ;
     end
   end
 end
