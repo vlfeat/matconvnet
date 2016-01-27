dc6f74966e55a5a5d9461b6f04b50f16e76d9bc6
Joao Henriques <jotaf98@hotmail.com>
2016-01-27 16:45:24 +0000

    Improved nnsolvers unit test, added DAG tests.

--- matlab/xtest/suite/nnsolvers.m
+++ matlab/xtest/suite/nnsolvers.m
@@ -1,7 +1,6 @@
 classdef nnsolvers < nntest
   properties (TestParameter)
-%     networkType = {'simplenn', 'dagnn'}
-    networkType = {'simplenn'}
+    networkType = {'simplenn', 'dagnn'}
     solver = {'sgd', 'adagrad', 'adadelta'}
   end
   properties
@@ -13,18 +12,15 @@ classdef nnsolvers < nntest
   methods (TestClassSetup)
     function data(test)
       % synthetic data, 2 classes of gaussian samples with different means
-      sz = [15, 10, 5] ;
-      mean1 = test.randn(sz) ;
-      mean1 = 2 * mean1 / norm(mean1(:)) ;
-      mean2 = test.randn(sz) ;
-      mean2 = 3 * mean2 / norm(mean2(:)) ;
+      test.range = 2 ;  % set standard deviation of test.randn()
+      sz = [15, 10, 5] ;  % input size
+      x1 = test.randn([sz, 100]) ;  % place mean at the origin
+      x2 = bsxfun(@plus, test.randn(sz), test.randn([sz, 100])) ;  % place mean randomly
       
-      x1 = bsxfun(@plus, mean1, test.randn([sz, 100])) ;
-      x2 = bsxfun(@plus, mean2, test.randn([sz, 100])) ;
       test.imdb.x = cat(4, x1, x2) ;
-      test.imdb.y = [test.ones(100, 1); -test.ones(100, 1)] ;
+      test.imdb.y = [test.ones(100, 1); 2 * test.ones(100, 1)] ;
       
-      test.init_w = test.randn([sz, 2]) ;  % initial parameters
+      test.init_w = 1e-3 * test.randn([sz, 2]) ;  % initial parameters
       test.init_b = test.zeros([2, 1]) ;
     end
   end
@@ -39,30 +35,40 @@ classdef nnsolvers < nntest
       net.layers = {struct('type','conv', 'weights',{{test.init_w, test.init_b}}), ...
                     struct('type','softmaxloss')} ;
       
-      switch networkType
-        case 'simplenn',
-          trainfn = @cnn_train ;
-        case 'dagnn',
-          trainfn = @cnn_train_dag ;
-          net = dagnn.DagNN.fromSimpleNN(net, 'canonicalNames', true) ;
-          net.addLayer('error', dagnn.Loss('loss', 'classerror'), ...
-                      {'prediction','label'}, 'error') ;
-      end
-
       switch test.currentDevice
         case 'cpu', gpus = [];
         case 'gpu', gpus = 1;
       end
 
+      switch networkType
+        case 'simplenn',
+          trainfn = @cnn_train ;
+          getBatch = @(imdb, batch) deal(imdb.x(:,:,:,batch), imdb.y(batch)) ;
+          
+        case 'dagnn',
+          trainfn = @cnn_train_dag ;
+          
+          if isempty(gpus)
+            getBatch = @(imdb, batch) ...
+                {'input',imdb.x(:,:,:,batch), 'label',imdb.y(batch)} ;
+          else
+            getBatch = @(imdb, batch) ...
+                {'input',gpuArray(imdb.x(:,:,:,batch)), 'label',imdb.y(batch)} ;
+          end
+          
+          net = dagnn.DagNN.fromSimpleNN(net, 'canonicalNames', true) ;
+          net.addLayer('error', dagnn.Loss('loss', 'classerror'), ...
+                      {'prediction','label'}, 'error') ;
+      end
+
       % train 1 epoch with small batches and check convergence
-      [~, info] = trainfn(net, test.imdb, ...
-        @(imdb, batch) deal(imdb.x(:,:,:,batch), imdb.y(batch)), ...
+      [~, info] = trainfn(net, test.imdb, getBatch, ...
         'train', 1:numel(test.imdb.y), 'val', 1, ...
         'solver',solver, 'batchSize', 10, 'numEpochs',1, 'continue', false, ...
         'gpus', gpus, 'plotStatistics', false) ;
 
       test.verifyLessThan(info.train.error(1), 0.3);
-      test.verifyLessThan(info.train.objective, 1.4e+5);
+      test.verifyLessThan(info.train.objective, 0.45);
     end
   end
 end
