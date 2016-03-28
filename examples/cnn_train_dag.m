b3d6b9340c17ec925deb1defa61e21e89dda871b
Assaf Mushinsky <assaf.mushinsky@eyesight-tech.com>
2016-03-28 13:28:12 +0300

    DagNN addLayer

--- examples/cnn_train_dag.m
+++ examples/cnn_train_dag.m
@@ -332,7 +332,7 @@ end
 % -------------------------------------------------------------------------
 function stats = extractStats(net)
 % -------------------------------------------------------------------------
-sel = find(cellfun(@(x) isa(x,'dagnn.Loss'), {net.layers.block})) ;
+sel = find(cellfun(@(x) isa(x,'dagnn.Loss') || isa(x,'dagnn.PDist'), {net.layers.block})) ;
 stats = struct() ;
 for i = 1:numel(sel)
   stats.(net.layers(sel(i)).outputs{1}) = net.layers(sel(i)).block.average ;
--- matlab/+dagnn/@DagNN/addLayer.m
+++ matlab/+dagnn/@DagNN/addLayer.m
@@ -13,10 +13,19 @@ index = find(strcmp(name, {obj.layers.name})) ;
 if ~isempty(index), error('There is already a layer with name ''%s''.', name), end
 index = numel(obj.layers) + 1 ;
 
+if nargin < 4, inputs = {} ; end
+if nargin < 5, outputs = {} ; end
 if nargin < 6, params = {} ; end
 if isstr(inputs), inputs = {inputs} ; end
 if isstr(outputs), outputs = {outputs} ; end
 if isstr(params), params = {params} ; end
+
+if isempty(inputs),
+    inputs = obj.layers(end).outputs; 
+end;
+if isempty(outputs),
+    outputs = { [name '_out'] }; 
+end;
 
 obj.layers(index) = struct(...
   'name', {name}, ...
