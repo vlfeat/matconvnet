b5b6a45ca1415ce0e60df594c11f1141fc17b826
Joao Henriques <jotaf98@hotmail.com>
2016-04-06 21:36:41 +0100

    allow learningRate and weightDecay properties in vl_nnconv. these are used in the automatically initialized Params, as expected. added vl_argparsepos function to help with parsing.

---
+++ matlab/autonn/vl_argparsepos.m
@@ -0,0 +1,43 @@
+function [opts, args] = vl_argparsepos(opts, args)
+%VL_ARGPARSEPOS
+%   Same as VL_ARGPARSE, but allows arbitrary positional arguments before
+%   the name-value pairs.
+%
+%   Example:
+%     opts.pad = 0 ;
+%     [opts, args] = vl_argparsepos(opts, {x, 'pad', 1, 'unknown', []}) ;
+%   The result will be:
+%     opts.pad = 1
+%     args = {x, 'unknown', []}
+
+  % even or odd indexes, always including the 2nd-to-last element of args
+  idx = (numel(args) - 1 : -2 : 1) ;  % reverse order
+  
+  if isempty(idx)
+    % no name-value pairs in the list
+    firstPair = numel(args) + 1 ;
+  else
+    % find first invalid name-value pair, starting from the end
+    pos = find(~cellfun(@ischar, args(idx)), 1) ;
+
+    if isempty(pos)  % all are valid
+      firstPair = idx(end) ;
+    else  % map back to argument indexes
+      firstPair = idx(pos) + 2 ;
+    end
+  end
+  
+  % separate them
+  namedArgs = args(firstPair:end) ;
+  posArgs = args(1:firstPair-1) ;
+
+  % call vl_argparse
+  if nargout == 2
+    [opts, namedArgs] = vl_argparse(opts, namedArgs) ;
+    args = [posArgs, namedArgs] ;  % back together
+  else
+    opts = vl_argparse(opts, namedArgs) ;
+  end
+  
+end
+
--- matlab/autonn/vl_nnconv_setup.m
+++ matlab/autonn/vl_nnconv_setup.m
@@ -1,32 +1,37 @@
 function inputs = vl_nnconv_setup(layer)
 %VL_NNCONV_SETUP
 %   Setup a conv or convt layer: if there is a 'size' argument,
-%   automatically initialize randomized Params for the filters and
-%   biases.
+%   automatically initialize randomized Params for the filters. Also
+%   handles 'hasBias' (initialize biases), 'learningRate' and 'weightDecay'
+%   arguments for the Params.
 
   assert(isequal(layer.func, @vl_nnconv) || isequal(layer.func, @vl_nnconvt)) ;
   
   inputs = layer.inputs ;
-
-  % parse 'size' argument
-  szPos = find(strcmp(inputs, 'size'), 1) ;
   
-  if ~isempty(szPos)
-    sz = inputs{szPos + 1} ;
-    scale = sqrt(2 / prod(sz(1:3))) ;
-    filters = Param('value', randn(sz, 'single') * scale) ;
+  % parse options. other options such as 'stride' will be maintained in the
+  % inputs list.
+  opts = struct('size', [], 'hasBias', false, 'learningRate', 1, 'weightDecay', 1) ;
+  
+  [opts, inputs] = vl_argparsepos(opts, inputs) ;
+  
+  if ~isempty(opts.size)
+    % a size was specified, create Params
+    scale = sqrt(2 / prod(opts.size(1:3))) ;
+    filters = Param('value', randn(opts.size, 'single') * scale, ...
+                    'learningRate', opts.learningRate(1), ...
+                    'weightDecay', opts.weightDecay(1)) ;
 
-    inputs(szPos : szPos + 1) = [] ;  % eliminate 'size' argument
-
-    % parse 'hasBias' argument
-    biasPos = find(strcmp(inputs, 'hasBias'), 1) ;
-    biases = [] ;
-    if isempty(biasPos) || ~inputs{szPos + 1}
-      biases = Param('value', zeros(sz(4), 1, 'single') * scale) ;
-      inputs(biasPos : biasPos + 1) = [] ;  % eliminate 'hasBias' argument
+    if opts.hasBias
+      % also create bias
+      biases = Param('value', zeros(sz(4), 1, 'single') * scale, ...
+                     'learningRate', opts.learningRate(max(1,end)), ...
+                     'weightDecay', opts.weightDecay(max(1,end))) ;
+    else
+      biases = [] ;
     end
 
+    %include filters and biases in inputs list
     inputs = [inputs(1), {filters, biases}, inputs(2:end)] ;
   end 
 end
-
