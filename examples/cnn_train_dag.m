5aff97632db01a9c8fbdc0fd4670a31df77d8aea
Tamir <tamir.anavi@eyesight-tech.com>
2016-04-12 11:12:01 +0300

    cnn_train_dag.m  - visualization using subplots  - save as .fig
    vl_nnpdist.m
     - do hinge only in backprog

--- examples/cnn_train_dag.m
+++ examples/cnn_train_dag.m
@@ -43,24 +43,24 @@ if isnan(opts.train), opts.train = [] ; end
 state.getBatch = getBatch ;
 evaluateMode = isempty(opts.train) ;
 if ~evaluateMode
-  if isempty(opts.derOutputs)
-    error('DEROUTPUTS must be specified when training.\n') ;
-  end
+    if isempty(opts.derOutputs)
+        error('DEROUTPUTS must be specified when training.\n') ;
+    end
 end
 stats = [] ;
 
 % setup GPUs
 numGpus = numel(opts.gpus) ;
 if numGpus > 1
-  if isempty(gcp('nocreate')),
-    parpool('local',numGpus) ;
-    spmd, gpuDevice(opts.gpus(labindex)), end
-  end
-  if exist(opts.memoryMapFile)
-    delete(opts.memoryMapFile) ;
-  end
+    if isempty(gcp('nocreate')),
+        parpool('local',numGpus) ;
+        spmd, gpuDevice(opts.gpus(labindex)), end
+    end
+    if exist(opts.memoryMapFile)
+        delete(opts.memoryMapFile) ;
+    end
 elseif numGpus == 1
-  gpuDevice(opts.gpus)
+    gpuDevice(opts.gpus)
 end
 
 % -------------------------------------------------------------------------
@@ -68,73 +68,79 @@ end
 % -------------------------------------------------------------------------
 
 modelPath = @(ep) fullfile(opts.expDir, sprintf('net-epoch-%d.mat', ep));
-modelFigPath = fullfile(opts.expDir, 'net-train.pdf') ;
+modelFigPath = fullfile(opts.expDir, 'net-train.fig') ;
+modelPdfPath = fullfile(opts.expDir, 'net-train.pdf') ;
 
 start = opts.continue * findLastCheckpoint(opts.expDir) ;
 if start >= 1
-  fprintf('%s: resuming by loading epoch %d\n', mfilename, start) ;
-  [net, stats] = loadState(modelPath(start)) ;
+    fprintf('%s: resuming by loading epoch %d\n', mfilename, start) ;
+    [net, stats] = loadState(modelPath(start)) ;
 end
 
 for epoch=start+1:opts.numEpochs
-
-  % train one epoch
-  state.epoch = epoch ;
-  state.learningRate = opts.learningRate(min(epoch, numel(opts.learningRate))) ;
-  state.train = opts.train(randperm(numel(opts.train))) ; % shuffle
-  state.val = opts.val ;
-  state.imdb = imdb ;
-
-  if numGpus <= 1
-    stats.train(epoch) = process_epoch(net, state, opts, 'train') ;
-    stats.val(epoch) = process_epoch(net, state, opts, 'val') ;
-  else
-    savedNet = net.saveobj() ;
-    spmd
-      net_ = dagnn.DagNN.loadobj(savedNet) ;
-      stats_.train = process_epoch(net_, state, opts, 'train') ;
-      stats_.val = process_epoch(net_, state, opts, 'val') ;
-      if labindex == 1, savedNet_ = net_.saveobj() ; end
-    end
-    net = dagnn.DagNN.loadobj(savedNet_{1}) ;
-    stats__ = accumulateStats(stats_) ;
-    stats.train(epoch) = stats__.train ;
-    stats.val(epoch) = stats__.val ;
-    clear net_ stats_ stats__ savedNet_ ;
-  end
-
-  if ~evaluateMode
-    saveState(modelPath(epoch), net, stats) ;
-  end
-
-  if opts.plotStatistics
-    figure(1) ; clf ;
-    plots = setdiff(...
-      cat(2,...
-      fieldnames(stats.train)', ...
-      fieldnames(stats.val)'), {'num', 'time'}) ;
-    for p = plots
-      p = char(p) ;
-      values = zeros(0, epoch) ;
-      leg = {} ;
-      for f = {'train', 'val'}
-        f = char(f) ;
-        if isfield(stats.(f), p)
-          tmp = [stats.(f).(p)] ;
-          values(end+1,:) = tmp(1,:)' ;
-          leg{end+1} = f ;
+    
+    % train one epoch
+    state.epoch = epoch ;
+    state.learningRate = opts.learningRate(min(epoch, numel(opts.learningRate))) ;
+    state.train = opts.train(randperm(numel(opts.train))) ; % shuffle
+    state.val = opts.val ;
+    state.imdb = imdb ;
+    
+    if numGpus <= 1
+        stats.train(epoch) = process_epoch(net, state, opts, 'train') ;
+        stats.val(epoch) = process_epoch(net, state, opts, 'val') ;
+    else
+        savedNet = net.saveobj() ;
+        spmd
+            net_ = dagnn.DagNN.loadobj(savedNet) ;
+            stats_.train = process_epoch(net_, state, opts, 'train') ;
+            stats_.val = process_epoch(net_, state, opts, 'val') ;
+            if labindex == 1, savedNet_ = net_.saveobj() ; end
         end
-      end
-      subplot(1,numel(plots),find(strcmp(p,plots))) ;
-      plot(1:epoch, values','o-') ;
-      xlabel('epoch') ;
-      title(p) ;
-      legend(leg{:}) ;
-      grid on ;
+        net = dagnn.DagNN.loadobj(savedNet_{1}) ;
+        stats__ = accumulateStats(stats_) ;
+        stats.train(epoch) = stats__.train ;
+        stats.val(epoch) = stats__.val ;
+        clear net_ stats_ stats__ savedNet_ ;
     end
-    drawnow ;
-    print(1, modelFigPath, '-dpdf') ;
-  end
+    
+    if ~evaluateMode
+        saveState(modelPath(epoch), net, stats) ;
+    end
+    
+    if opts.plotStatistics
+        figure(1) ; maximize(1); clf ; 
+        plots = setdiff(...
+            cat(2,...
+            fieldnames(stats.train)', ...
+            fieldnames(stats.val)'), {'num', 'time'}) ;
+        n = ceil(sqrt(numel(plots)));
+        m = ceil(numel(plots)/n);
+        
+        for p = plots
+            p = char(p) ;
+            values = zeros(0, epoch) ;
+            leg = {} ;
+            for f = {'train', 'val'}
+                f = char(f) ;
+                if isfield(stats.(f), p)
+                    tmp = [stats.(f).(p)] ;
+                    values(end+1,:) = tmp(1,:)' ;
+                    leg{end+1} = f ;
+                end
+            end
+            subplot(n,m,find(strcmp(p,plots))) ;
+            plot(1:epoch, values','o-') ;
+            xlabel('epoch') ;
+            title(p,'Interpreter','none') ;
+            l=legend(leg{:}) ;
+            set(l,'Interpreter','none');
+            grid on ;
+        end
+        drawnow ;
+        print(1, modelPdfPath, '-dpdf');
+        saveas(1, modelFigPath);
+    end
 end
 
 % -------------------------------------------------------------------------
@@ -142,20 +148,20 @@ function stats = process_epoch(net, state, opts, mode)
 % -------------------------------------------------------------------------
 
 if strcmp(mode,'train')
-  state.momentum = num2cell(zeros(1, numel(net.params))) ;
+    state.momentum = num2cell(zeros(1, numel(net.params))) ;
 end
 
 numGpus = numel(opts.gpus) ;
 if numGpus >= 1
-  net.move('gpu') ;
-  if strcmp(mode,'train')
-    state.momentum = cellfun(@gpuArray,state.momentum,'UniformOutput',false) ;
-  end
+    net.move('gpu') ;
+    if strcmp(mode,'train')
+        state.momentum = cellfun(@gpuArray,state.momentum,'UniformOutput',false) ;
+    end
 end
 if numGpus > 1
-  mmap = map_gradients(opts.memoryMapFile, net, numGpus) ;
+    mmap = map_gradients(opts.memoryMapFile, net, numGpus) ;
 else
-  mmap = [] ;
+    mmap = [] ;
 end
 
 stats.time = 0 ;
@@ -165,67 +171,67 @@ start = tic ;
 num = 0 ;
 
 for t=1:opts.batchSize:numel(subset)
-  batchSize = min(opts.batchSize, numel(subset) - t + 1) ;
-
-  for s=1:opts.numSubBatches
-    % get this image batch and prefetch the next
-    batchStart = t + (labindex-1) + (s-1) * numlabs ;
-    batchEnd = min(t+opts.batchSize-1, numel(subset)) ;
-    batch = subset(batchStart : opts.numSubBatches * numlabs : batchEnd) ;
-    num = num + numel(batch) ;
-    if numel(batch) == 0, continue ; end
-
-    inputs = state.getBatch(state.imdb, batch) ;
-
-    if opts.prefetch
-      if s == opts.numSubBatches
-        batchStart = t + (labindex-1) + opts.batchSize ;
-        batchEnd = min(t+2*opts.batchSize-1, numel(subset)) ;
-      else
-        batchStart = batchStart + numlabs ;
-      end
-      nextBatch = subset(batchStart : opts.numSubBatches * numlabs : batchEnd) ;
-      state.getBatch(state.imdb, nextBatch) ;
+    batchSize = min(opts.batchSize, numel(subset) - t + 1) ;
+    
+    for s=1:opts.numSubBatches
+        % get this image batch and prefetch the next
+        batchStart = t + (labindex-1) + (s-1) * numlabs ;
+        batchEnd = min(t+opts.batchSize-1, numel(subset)) ;
+        batch = subset(batchStart : opts.numSubBatches * numlabs : batchEnd) ;
+        num = num + numel(batch) ;
+        if numel(batch) == 0, continue ; end
+        
+        inputs = state.getBatch(state.imdb, batch) ;
+        
+        if opts.prefetch
+            if s == opts.numSubBatches
+                batchStart = t + (labindex-1) + opts.batchSize ;
+                batchEnd = min(t+2*opts.batchSize-1, numel(subset)) ;
+            else
+                batchStart = batchStart + numlabs ;
+            end
+            nextBatch = subset(batchStart : opts.numSubBatches * numlabs : batchEnd) ;
+            state.getBatch(state.imdb, nextBatch) ;
+        end
+        
+        if strcmp(mode, 'train')
+            net.mode = 'normal' ;
+            net.accumulateParamDers = (s ~= 1) ;
+            net.eval(inputs, opts.derOutputs) ;
+        else
+            net.mode = 'test' ;
+            net.eval(inputs) ;
+        end
     end
-
+    
+    % extract learning stats
+    stats = opts.extractStatsFn(net) ;
+    
+    % accumulate gradient
     if strcmp(mode, 'train')
-      net.mode = 'normal' ;
-      net.accumulateParamDers = (s ~= 1) ;
-      net.eval(inputs, opts.derOutputs) ;
-    else
-      net.mode = 'test' ;
-      net.eval(inputs) ;
+        if ~isempty(mmap)
+            write_gradients(mmap, net) ;
+            labBarrier() ;
+        end
+        state = accumulate_gradients(state, net, opts, batchSize, mmap) ;
     end
-  end
-
-  % extract learning stats
-  stats = opts.extractStatsFn(net) ;
-
-  % accumulate gradient
-  if strcmp(mode, 'train')
-    if ~isempty(mmap)
-      write_gradients(mmap, net) ;
-      labBarrier() ;
+    
+    % print learning statistics
+    time = toc(start) ;
+    stats.num = num ;
+    stats.time = toc(start) ;
+    
+    fprintf('%s: epoch %02d: %3d/%3d: %.1f Hz', ...
+        mode, ...
+        state.epoch, ...
+        fix(t/opts.batchSize)+1, ceil(numel(subset)/opts.batchSize), ...
+        stats.num/stats.time * max(numGpus, 1)) ;
+    for f = setdiff(fieldnames(stats)', {'num', 'time'})
+        f = char(f) ;
+        fprintf(' %s:', f) ;
+        fprintf(' %.3f', stats.(f)) ;
     end
-    state = accumulate_gradients(state, net, opts, batchSize, mmap) ;
-  end
-
-  % print learning statistics
-  time = toc(start) ;
-  stats.num = num ;
-  stats.time = toc(start) ;
-
-  fprintf('%s: epoch %02d: %3d/%3d: %.1f Hz', ...
-    mode, ...
-    state.epoch, ...
-    fix(t/opts.batchSize)+1, ceil(numel(subset)/opts.batchSize), ...
-    stats.num/stats.time * max(numGpus, 1)) ;
-  for f = setdiff(fieldnames(stats)', {'num', 'time'})
-    f = char(f) ;
-    fprintf(' %s:', f) ;
-    fprintf(' %.3f', stats.(f)) ;
-  end
-  fprintf('\n') ;
+    fprintf('\n') ;
 end
 
 net.reset() ;
@@ -235,40 +241,40 @@ net.move('cpu') ;
 function state = accumulate_gradients(state, net, opts, batchSize, mmap)
 % -------------------------------------------------------------------------
 for p=1:numel(net.params)
-
-  % bring in gradients from other GPUs if any
-  if ~isempty(mmap)
-    numGpus = numel(mmap.Data) ;
-    tmp = zeros(size(mmap.Data(labindex).(net.params(p).name)), 'single') ;
-    for g = setdiff(1:numGpus, labindex)
-      tmp = tmp + mmap.Data(g).(net.params(p).name) ;
+    
+    % bring in gradients from other GPUs if any
+    if ~isempty(mmap)
+        numGpus = numel(mmap.Data) ;
+        tmp = zeros(size(mmap.Data(labindex).(net.params(p).name)), 'single') ;
+        for g = setdiff(1:numGpus, labindex)
+            tmp = tmp + mmap.Data(g).(net.params(p).name) ;
+        end
+        net.params(p).der = net.params(p).der + tmp ;
+    else
+        numGpus = 1 ;
     end
-    net.params(p).der = net.params(p).der + tmp ;
-  else
-    numGpus = 1 ;
-  end
-
-  switch net.params(p).trainMethod
-
-    case 'average' % mainly for batch normalization
-      thisLR = net.params(p).learningRate ;
-      net.params(p).value = ...
-          (1 - thisLR) * net.params(p).value + ...
-          (thisLR/batchSize/net.params(p).fanout) * net.params(p).der ;
-
-    case 'gradient'
-      thisDecay = opts.weightDecay * net.params(p).weightDecay ;
-      thisLR = state.learningRate * net.params(p).learningRate ;
-      state.momentum{p} = opts.momentum * state.momentum{p} ...
-        - thisDecay * net.params(p).value ...
-        - (1 / batchSize) * net.params(p).der ;
-      net.params(p).value = net.params(p).value + thisLR * state.momentum{p} ;
-
-    case 'otherwise'
-      error('Unknown training method ''%s'' for parameter ''%s''.', ...
-        net.params(p).trainMethod, ...
-        net.params(p).name) ;
-  end
+    
+    switch net.params(p).trainMethod
+        
+        case 'average' % mainly for batch normalization
+            thisLR = net.params(p).learningRate ;
+            net.params(p).value = ...
+                (1 - thisLR) * net.params(p).value + ...
+                (thisLR/batchSize/net.params(p).fanout) * net.params(p).der ;
+            
+        case 'gradient'
+            thisDecay = opts.weightDecay * net.params(p).weightDecay ;
+            thisLR = state.learningRate * net.params(p).learningRate ;
+            state.momentum{p} = opts.momentum * state.momentum{p} ...
+                - thisDecay * net.params(p).value ...
+                - (1 / batchSize) * net.params(p).der ;
+            net.params(p).value = net.params(p).value + thisLR * state.momentum{p} ;
+            
+        case 'otherwise'
+            error('Unknown training method ''%s'' for parameter ''%s''.', ...
+                net.params(p).trainMethod, ...
+                net.params(p).name) ;
+    end
 end
 
 % -------------------------------------------------------------------------
@@ -276,17 +282,17 @@ function mmap = map_gradients(fname, net, numGpus)
 % -------------------------------------------------------------------------
 format = {} ;
 for i=1:numel(net.params)
-  format(end+1,1:3) = {'single', size(net.params(i).value), net.params(i).name} ;
+    format(end+1,1:3) = {'single', size(net.params(i).value), net.params(i).name} ;
 end
 format(end+1,1:3) = {'double', [3 1], 'errors'} ;
 if ~exist(fname) && (labindex == 1)
-  f = fopen(fname,'wb') ;
-  for g=1:numGpus
-    for i=1:size(format,1)
-      fwrite(f,zeros(format{i,2},format{i,1}),format{i,1}) ;
+    f = fopen(fname,'wb') ;
+    for g=1:numGpus
+        for i=1:size(format,1)
+            fwrite(f,zeros(format{i,2},format{i,1}),format{i,1}) ;
+        end
     end
-  end
-  fclose(f) ;
+    fclose(f) ;
 end
 labBarrier() ;
 mmap = memmapfile(fname, 'Format', format, 'Repeat', numGpus, 'Writable', true) ;
@@ -295,7 +301,7 @@ mmap = memmapfile(fname, 'Format', format, 'Repeat', numGpus, 'Writable', true) 
 function write_gradients(mmap, net)
 % -------------------------------------------------------------------------
 for i=1:numel(net.params)
-  mmap.Data(labindex).(net.params(i).name) = gather(net.params(i).der) ;
+    mmap.Data(labindex).(net.params(i).name) = gather(net.params(i).der) ;
 end
 
 % -------------------------------------------------------------------------
@@ -305,28 +311,28 @@ function stats = accumulateStats(stats_)
 stats = struct() ;
 
 for s = {'train', 'val'}
-  s = char(s) ;
-  total = 0 ;
-
-  for g = 1:numel(stats_)
-    stats__ = stats_{g} ;
-    num__ = stats__.(s).num ;
-    total = total + num__ ;
-
-    for f = setdiff(fieldnames(stats__.(s))', 'num')
-      f = char(f) ;
-
-      if g == 1
-        stats.(s).(f) = 0 ;
-      end
-      stats.(s).(f) = stats.(s).(f) + stats__.(s).(f) * num__ ;
-
-      if g == numel(stats_)
-        stats.(s).(f) = stats.(s).(f) / total ;
-      end
+    s = char(s) ;
+    total = 0 ;
+    
+    for g = 1:numel(stats_)
+        stats__ = stats_{g} ;
+        num__ = stats__.(s).num ;
+        total = total + num__ ;
+        
+        for f = setdiff(fieldnames(stats__.(s))', 'num')
+            f = char(f) ;
+            
+            if g == 1
+                stats.(s).(f) = 0 ;
+            end
+            stats.(s).(f) = stats.(s).(f) + stats__.(s).(f) * num__ ;
+            
+            if g == numel(stats_)
+                stats.(s).(f) = stats.(s).(f) / total ;
+            end
+        end
     end
-  end
-  stats.(s).num = total ;
+    stats.(s).num = total ;
 end
 
 % -------------------------------------------------------------------------
@@ -335,7 +341,7 @@ function stats = extractStats(net)
 sel = find(cellfun(@(x) isa(x,'dagnn.Loss') || isa(x,'dagnn.PDist'), {net.layers.block})) ;
 stats = struct() ;
 for i = 1:numel(sel)
-  stats.(net.layers(sel(i)).outputs{1}) = net.layers(sel(i)).block.average ;
+    stats.(net.layers(sel(i)).outputs{1}) = net.layers(sel(i)).block.average ;
 end
 
 % -------------------------------------------------------------------------
--- matlab/vl_nnpdist.m
+++ matlab/vl_nnpdist.m
@@ -67,7 +67,7 @@ end
 
 d = bsxfun(@minus, x, x0) ;
 d(isnan(x0)) = 0;
-d(abs(d) < opts.hinge) = 0;
+if backMode, d(abs(d) < opts.hinge) = 0; end;
 if opts.normed, d = bsxfun(@rdivide, d, (x0+~x0)); end;
 
 if ~opts.noRoot
