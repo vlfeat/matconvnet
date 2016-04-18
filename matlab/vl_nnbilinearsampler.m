9f8004654c7f427c9380b874a3863ca0bdf6d70e
Ankush Gupta <ankush@robots.ox.ac.uk>
2016-04-18 20:02:08 +0100

    updated docs and added files to vl_compilenn

--- matlab/vl_nnbilinearsampler.m
+++ matlab/vl_nnbilinearsampler.m
@@ -7,14 +7,16 @@
 %   height and width of the feature-map, D is the number of channels,
 %   and Ni, the number of images in the stack.
 %
-%   GRID is an array of dimension Ho x Wo x 2 x Ng, where (Ho,Wo) are
+%   GRID is an array of dimension 2 x Ho x Wo x Ng, where (Ho,Wo) are
 %   the height and width of the output feature-map, 2 is for the two
 %   spatial dimensions corresponding to Ho and Wo respectively, and Ng
-%   is the number of transforms.  (Ho,Wo) need to be equal to (Hi,Wi).
+%   is the number of transforms. (Ho,Wo) need not be equal to (Hi,Wi).
 %   Further, Ng can be a multiple of Ni: in this case, it is assumed
 %   that there are Ng/Ni transforms per input image, hence, the
-%   transforms [1 ... Ng/Ni] are applied to the first image, [Ng/Ni+1
-%   ... 2*Ng/Ni] are applied to the second image, etc.
+%   transforms [1 ... Ng/Ni] are applied to the first image,
+%   [Ng/Ni+1 ... 2*Ng/Ni] are applied to the second image, etc.
+%   The grid coordinates are normalized such that the extents of the
+%   output and input image are both in [-,1,1] (see the paper for details).
 %
 %   Y is an array of size Ho x Wo x D x Ng. Note, the SAME transform
 %   is applied to all the D channels of a given image.
@@ -31,7 +33,7 @@
 %   You can use the 'NoCudnn' option to disable cuDNN or 'Cudnn'
 %   to activate it back again
 %   (the choice sticks until MATLAB purges the MEX files for any reason).
-
+%
 % Copyright (C) 2016 Ankush Gupta, Andrea Vedaldi.
 % All rights reserved.
 %
