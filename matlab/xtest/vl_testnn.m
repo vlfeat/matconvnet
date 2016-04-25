2a1d685773adc123daa8fc21290fb001bcaa3658
Ankush Gupta <ankush@robots.ox.ac.uk>
2016-04-25 20:22:37 +0100

    clean-up

--- matlab/xtest/vl_testnn.m
+++ matlab/xtest/vl_testnn.m
@@ -1,4 +1,4 @@
-function result = vl_testnn(varargin)
+function vl_testnn(varargin)
 %VL_TESTNN Run MatConvNet test suite 
 % VL_TESTNN('option', value, ...) takes the following options:
 %  `cpu`:: true
@@ -29,8 +29,8 @@ function result = vl_testnn(varargin)
 
 opts.cpu = true ;
 opts.gpu = false ;
-opts.single = false;
-opts.double = true ;
+opts.single = true;
+opts.double = false ;
 opts.command = 'nn' ;
 opts.break = false ;
 opts.tapFile = '';
