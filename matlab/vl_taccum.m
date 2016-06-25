185a26559313d0aeb1074bb6bc79b04ef53567a6
Andrea Vedaldi <vedaldi@gmail.com>
2016-06-25 23:21:48 +0100

    doc: adds new functions

--- matlab/vl_taccum.m
+++ matlab/vl_taccum.m
@@ -1,7 +1,7 @@
 function a = vl_taccum(alpha, a, beta, b)
 %VL_TACCUM  Compute A = alpha A + beta B
 %   A = VL_TACCUM(ALPHA, A, BETA, B) computes efficiently A = alpha A
-%   + beta B. For GPU array, it performs its computation in place, by
+%   + beta B. For GPU arrays, it performs its computation in place, by
 %   modifiying A without creating an additional copy.
 
 % Copyright (C) 2016 Andrea Vedaldi.
