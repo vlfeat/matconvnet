747fc93a3e538d7d5e73d4c9a22a3441b0379748
Andrea Vedaldi <vedaldi@gmail.com>
2016-08-25 09:39:21 +0200

    examples: cleanup

--- examples/imagenet/cnn_imagenet_minimal.m
+++ examples/imagenet/cnn_imagenet_minimal.m
@@ -1,28 +1,31 @@
 function cnn_imagenet_minimal()
 % CNN_IMAGENET_MINIMAL   Minimalistic demonstration of how to run an ImageNet CNN model
 
-% setup toolbox
+% Setup MatConvNet.
 run(fullfile(fileparts(mfilename('fullpath')), ...
   '..', '..', 'matlab', 'vl_setupnn.m')) ;
 
-% download a pre-trained CNN from the web
+% Download a pre-trained CNN from the web.
 if ~exist('imagenet-vgg-f.mat', 'file')
-  fprintf('Downloading a model ... this may take a while\n') ;
+  fprintf('Downloading the VGG-F model ... this may take a while\n') ;
   urlwrite('http://www.vlfeat.org/matconvnet/models/imagenet-vgg-f.mat', ...
     'imagenet-vgg-f.mat') ;
 end
-net = load('imagenet-vgg-f.mat') ;
 
-% obtain and preprocess an image
+% Load the model and upgrade it to MatConvNet current version.
+net = load('imagenet-vgg-f.mat') ;
+net = vl_simplenn_tidy(net) ;
+
+% Obtain and preprocess an image.
 im = imread('peppers.png') ;
 im_ = single(im) ; % note: 255 range
 im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
 im_ = im_ - net.meta.normalization.averageImage ;
 
-% run the CNN
+% Run the CNN.
 res = vl_simplenn(net, im_) ;
 
-% show the classification result
+% Show the classification result.
 scores = squeeze(gather(res(end).x)) ;
 [bestScore, best] = max(scores) ;
 figure(1) ; clf ; imagesc(im) ;
