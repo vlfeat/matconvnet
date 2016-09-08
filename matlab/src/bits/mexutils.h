328cd2f2e6ff2dc57f310aef35d11884bb71216b
Karel Lenc <lenckarel@gmail.com>
2016-09-08 15:29:38 +0100

    Fixed issue #677.
    Affected only Win platform, ssize_t incorrectly defined
    as unsigned, messed-up the type in a mex header.

--- matlab/src/bits/mexutils.h
+++ matlab/src/bits/mexutils.h
@@ -29,9 +29,9 @@ the terms of the BSD license (see the COPYING file).
 #define snprintf _snprintf
 #define vsnprintf _vsnprintf
 #ifdef  _WIN64
-typedef unsigned __int64 ssize_t;
+typedef signed __int64 ssize_t;
 #else
-typedef unsigned int ssize_t;
+typedef signed int ssize_t;
 #endif
 #if _MSC_VER < 1800
 // Add some missing functions from C99
