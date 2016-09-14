33249934ffc595ca98decc247309177d57b84d26
Karel Lenc <lenckarel@gmail.com>
2016-09-14 11:29:39 +0100

    Fixed issue #688.
    VS2012 apparently has the bools defined.
    Removed the vl_tmove('reset') - not on windows.

--- matlab/src/bits/mexutils.h
+++ matlab/src/bits/mexutils.h
@@ -24,7 +24,7 @@ the terms of the BSD license (see the COPYING file).
 #include <string.h>
 #include <assert.h>
 
-#if defined(_MSC_VER) && _MSC_VER < 1800
+#if defined(_MSC_VER) && _MSC_VER < 1700
 #define false 0
 #define true 1
 #else
