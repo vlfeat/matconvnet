dee9e2d3980375d9789ae963da98b16276a58053
Itay Boneh <itay.boneh@eyesight-tech.com>
2016-09-25 17:07:23 +0300

    Revert support for specific compiler (added by matconvnet).

--- matlab/vl_compilenn.m
+++ matlab/vl_compilenn.m
@@ -160,7 +160,6 @@ opts.debug            = false;
 opts.cudaMethod       = [] ;
 opts.cudaRoot         = [] ;
 opts.cudaArch         = [] ;
-opts.cudaCompilerBinDir = [] ;
 opts.defCudaArch      = [...
   '-gencode=arch=compute_20,code=\"sm_20,compute_20\" '...
   '-gencode=arch=compute_30,code=\"sm_30,compute_30\"'];
@@ -529,15 +528,9 @@ mex(mopts{:}) ;
 function nvcc_compile(opts, src, tgt, nvcc_opts)
 % --------------------------------------------------------------------
 nvcc_path = fullfile(opts.cudaRoot, 'bin', 'nvcc');
-if isempty(opts.cudaCompilerBinDir)
-    nvcc_cmd = sprintf('"%s" -c "%s" %s -o "%s"', ...
-        nvcc_path, src, ...
-        strjoin(nvcc_opts), tgt);
-else
-    nvcc_cmd = sprintf('"%s" -c "%s" %s -o "%s" --compiler-bindir "%s"', ...
-        nvcc_path, src, ...
-        strjoin(nvcc_opts), tgt, opts.cudaCompilerBinDir);
-end
+nvcc_cmd = sprintf('"%s" -c "%s" %s -o "%s"', ...
+                   nvcc_path, src, ...
+                   strjoin(nvcc_opts), tgt);
 opts.verbose && fprintf('%s: NVCC CC: %s\n', mfilename, nvcc_cmd) ;
 status = system(nvcc_cmd);
 if status, error('Command %s failed.', nvcc_cmd); end;
