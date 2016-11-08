f8c9e8e55f4f0ed7cdf07332b556e86ba75d21fc
Joao Henriques <jotaf98@hotmail.com>
2016-11-08 19:21:23 +0000

    improved net.displayVars, with links to forward/backward structures

--- matlab/autonn/@Net/displayVars.m
+++ matlab/autonn/@Net/displayVars.m
@@ -41,6 +41,19 @@ info = net.getVarsInfo() ;
 assert(numel(info) == numel(vars)) ;
 
 
+if opts.showLinks
+  if ~isempty(inputname(1))
+    netname = inputname(1) ;
+  else  % unnamed Net object
+    netname = 'net' ;
+  end
+  if nargin >= 2 && ~isempty(inputname(2))  % a variables list was given
+    varname = inputname(2) ;
+  else  % only a Net object was given
+    varname = [netname '.vars'] ;
+  end
+end
+
 % print information for each variable
 funcs = cell(numel(vars), 1) ;
 values = cell(numel(vars), 1) ;
@@ -54,6 +67,18 @@ for i = 1:numel(vars)
     funcs{i} = func2str(net.forward(info(i).index).func) ;
     if info(i).outputArgPos > 1
       funcs{i} = sprintf('%s (output #%i)', funcs{i}, info(i).outputArgPos) ;
+    end
+  
+    if opts.showLinks
+      % link to get to the originating layer (fwd/bwd structs).
+      % note that the links must have a fixed number of characters,
+      % otherwise the columns will be misaligned
+      fwd = sprintf('%s.forward(%i)', netname, info(i).index) ;
+      bwd = sprintf('%s.backward(%i)', netname, numel(net.forward) - info(i).index + 1) ;
+      
+      funcs{i} = sprintf(['<a href="matlab:if exist(''%s'',''var''),disp(''%s ='');disp(%s);' ...
+        'disp(''%s ='');disp(%s);else,disp(''%s, %s'');end">%s</a>'], ...
+        netname, fwd, fwd, bwd, bwd, fwd, bwd, funcs{i}) ;
     end
   else
     % other var types like 'param' or 'input' will be displayed as such
@@ -69,6 +94,12 @@ for i = 1:numel(vars)
   end
   str = [str ' ' sprintf('%ix', size(v))] ;  %#ok<*AGROW>
   values{i} = str(1:end-1) ;  % remove extraneous 'x' at the end of the var size
+  
+  if opts.showLinks
+    % link to get variable's value
+    values{i} = sprintf('<a href="matlab:display(''%s{%d}'')">%s</a>', ...
+      varname, i, values{i}) ;
+  end
   
   % flags (GPU, NaN, Inf)
   str = '' ;
@@ -100,58 +131,55 @@ end
 
 if opts.showRange  % convert to string, filling invalid values with spaces
   minStr(validRange,:) = num2str(mins(validRange), '%.2g') ;
-  maxStr(validRange,:) = num2str(maxs(validRange), '%.2g') ;
   minStr(~validRange,:) = ' ' ;
+  minStr = num2cell(minStr, 2) ;
+  maxStr(validRange,:) = num2str(maxs(validRange), '%.2g') ;
   maxStr(~validRange,:) = ' ' ;
+  maxStr = num2cell(maxStr, 2) ;
 end
 
 
-if opts.showLinks
-  if nargin >= 2 && ~isempty(inputname(2))  % a variables list was given
-    varname = inputname(2) ;
-  elseif ~isempty(inputname(1))  % only a Net object was given
-    varname = [inputname(1) '.vars'] ;
-  else  % unnamed Net object
-    varname = 'net.vars' ;
-  end
-  % note that the links must have a fixed number of characters, otherwise
-  % the columns will be misaligned
-  link = @(i) sprintf('<a href="matlab:display(''%s{%3d}'')">%s</a>', ...
-    varname, i, values{i}) ;
-  values = arrayfun(@(i) {link(i)}, 1:numel(values)) ;
-end
-idx = arrayfun(@(i) {num2str(i)}, 1:2:numel(info)-1) ;
+idx = arrayfun(@(i) {num2str(i)}, (1:2:numel(info)-1)') ;
 
 % now print out the info as a table
-str = repmat(' ', numel(info) / 2 + 1, 1) ;
-str = [str, char('Idx', idx{:})] ;
-str(:,end+1:end+2) = ' ' ;
-str = [str, char('Function', funcs{1:2:end-1})] ;
-str(:,end+1:end+2) = ' ' ;
-str = [str, char('Name', info(1:2:end-1).name)] ;
+table = [{'Idx', 'Function', 'Name'};
+  idx, funcs(1:2:end-1), {info(1:2:end-1).name}'] ;
 
 % repeat same set of columns for value and der (size/class/flags/min/max)
 headers = {'Value', 'Derivative'} ;
 
-% create dummy links in the headers, to align them nicely
-if opts.showLinks
-  spaces = numel(['display(''' varname '{   }'')']);
-  headers = cellfun(@(name) {[name '<a href="matlab:' blanks(spaces) '"></a>']}, headers);
-end
-
 for i = 1:2
   idx = i : 2 : numel(values) - 2 + i ;  % odd or even elements, respectively
-  str(:,end+1:end+2) = ' ' ;
-  str = [str, char(headers{i}, values{idx})] ;
-  str(:,end+1:end+2) = ' ' ;
-  str = [str, char('Flags', flags{idx})] ;
-  if opts.showRange
-    str(:,end+1:end+2) = ' ' ;
-    str = [str, char('Min', minStr(idx,:))] ;
-    str(:,end+1:end+2) = ' ' ;
-    str = [str, char('Max', maxStr(idx,:))] ;
-  end
+  t = [{headers{i}, 'Flags', 'Min', 'Max'};
+    values(idx), flags(idx), minStr(idx), maxStr(idx)] ;
+  table = [table, t] ;
 end
 
-disp(str) ;
+% align column contents
+for i = 1:size(table,2)
+  table(:,i) = leftAlign(table(:,i)) ;
 end
+
+% add spaces between columns
+t = cell(size(table,1), size(table,2) * 2) ;
+t(:,1:2:end) = {'  '} ;
+t(:,2:2:end) = table ;
+table = t ;
+
+% concatenate and display
+for i = 1:size(table, 1)
+  disp([table{i,:}]) ;
+end
+fprintf('\n') ;
+
+end
+
+function str = leftAlign(str)
+  % aligns cell array of strings to the left, as a column, ignoring links
+  strNoLinks = regexprep(str, '<[^>]*>', '') ;  % remove links
+  lengths = cellfun('length', strNoLinks) ;
+  numBlanks = max(lengths) - lengths ;
+  str = cellfun(@(s, n) {[s blanks(n)]}, str, num2cell(numBlanks)) ;
+end
+
+
