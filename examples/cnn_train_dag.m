98833472f8b9772d2b02941a089cfecf27c65a39
Joao Henriques <jotaf98@hotmail.com>
2017-01-27 22:17:48 +0000

    allow non-numeric train/val set vectors (e.g. cell arrays)

--- examples/cnn_train_dag.m
+++ examples/cnn_train_dag.m
@@ -51,8 +51,8 @@ opts = vl_argparse(opts, varargin) ;
 if ~exist(opts.expDir, 'dir'), mkdir(opts.expDir) ; end
 if isempty(opts.train), opts.train = find(imdb.images.set==1) ; end
 if isempty(opts.val), opts.val = find(imdb.images.set==2) ; end
-if isnan(opts.train), opts.train = [] ; end
-if isnan(opts.val), opts.val = [] ; end
+if isnumeric(opts.train) && isnan(opts.train), opts.train = [] ; end
+if isnumeric(opts.train) && isnan(opts.val), opts.val = [] ; end
 
 % -------------------------------------------------------------------------
 %                                                            Initialization
