b4c57a0ac9b3cb53c7d16a2ea4404401891dc092
Joao Henriques <jotaf98@hotmail.com>
2017-02-09 14:50:14 +0000

    Fix bug in support for empty train/val sets

--- examples/cnn_train_dag.m
+++ examples/cnn_train_dag.m
@@ -49,8 +49,8 @@ opts = vl_argparse(opts, varargin) ;
 if ~exist(opts.expDir, 'dir'), mkdir(opts.expDir) ; end
 if isempty(opts.train), opts.train = find(imdb.images.set==1) ; end
 if isempty(opts.val), opts.val = find(imdb.images.set==2) ; end
-if isnan(opts.train), opts.train = [] ; end
-if isnan(opts.val), opts.val = [] ; end
+if isequal(opts.train, NaN), opts.train = [] ; end
+if isequal(opts.val, NaN), opts.val = [] ; end
 
 % -------------------------------------------------------------------------
 %                                                            Initialization
