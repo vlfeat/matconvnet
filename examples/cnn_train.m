vlfeat/matconvnet

commit b4c57a0ac9b3cb53c7d16a2ea4404401891dc092

Fix bug in support for empty train/val sets

examples/cnn_train.m
@@ -63,8 +63,8 @@ opts = vl_argparse(opts, varargin) ;
 if ~exist(opts.expDir, 'dir'), mkdir(opts.expDir) ; end
 if isempty(opts.train), opts.train = find(imdb.images.set==1) ; end
 if isempty(opts.val), opts.val = find(imdb.images.set==2) ; end
-if isnan(opts.train), opts.train = [] ; end
-if isnan(opts.val), opts.val = [] ; end
+if isequal(opts.train, NaN), opts.train = [] ; end
+if isequal(opts.val, NaN), opts.val = [] ; end
 
 % -------------------------------------------------------------------------
 %                                                            Initialization

examples/cnn_train_dag.m
@@ -49,8 +49,8 @@ opts = vl_argparse(opts, varargin) ;
 if ~exist(opts.expDir, 'dir'), mkdir(opts.expDir) ; end
 if isempty(opts.train), opts.train = find(imdb.images.set==1) ; end
 if isempty(opts.val), opts.val = find(imdb.images.set==2) ; end
-if isnan(opts.train), opts.train = [] ; end
-if isnan(opts.val), opts.val = [] ; end
+if isequal(opts.train, NaN), opts.train = [] ; end
+if isequal(opts.val, NaN), opts.val = [] ; end
 
 % -------------------------------------------------------------------------
 %                                                            Initialization

commit 1505d504b87a4cd8874d32384e3b774a2aa19727

Fixed a bug introduced in b4c57a0, isequal(~,nan) is always false.

examples/cnn_train.m
@@ -63,8 +63,12 @@ opts = vl_argparse(opts, varargin) ;
 if ~exist(opts.expDir, 'dir'), mkdir(opts.expDir) ; end
 if isempty(opts.train), opts.train = find(imdb.images.set==1) ; end
 if isempty(opts.val), opts.val = find(imdb.images.set==2) ; end
-if isequal(opts.train, NaN), opts.train = [] ; end
-if isequal(opts.val, NaN), opts.val = [] ; end
+if isscalar(opts.train) && isnumeric(opts.train) && isnan(opts.train)
+  opts.train = [] ;
+end
+if isscalar(opts.val) && isnumeric(opts.val) && isnan(opts.val)
+  opts.val = [] ;
+end
 
 % -------------------------------------------------------------------------
 %                                                            Initialization

examples/cnn_train_dag.m
@@ -48,8 +48,12 @@ opts = vl_argparse(opts, varargin) ;
 if ~exist(opts.expDir, 'dir'), mkdir(opts.expDir) ; end
 if isempty(opts.train), opts.train = find(imdb.images.set==1) ; end
 if isempty(opts.val), opts.val = find(imdb.images.set==2) ; end
-if isequal(opts.train, NaN), opts.train = [] ; end
-if isequal(opts.val, NaN), opts.val = [] ; end
+if isscalar(opts.train) && isnumeric(opts.train) && isnan(opts.train)
+  opts.train = [] ;
+end
+if isscalar(opts.val) && isnumeric(opts.val) && isnan(opts.val)
+  opts.val = [] ;
+end
 
 % -------------------------------------------------------------------------
 %                                                            Initialization