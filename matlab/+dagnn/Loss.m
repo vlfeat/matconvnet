cfc2c47297ee1482e9dfc921e4fbf492247e1454
Karel Lenc <karel@robots.ox.ac.uk>
2017-03-16 16:43:14 +0000

    Added a 'display' parameter to dagnn.Loss. Moved accummulation to a separate method.

--- matlab/+dagnn/Loss.m
+++ matlab/+dagnn/Loss.m
@@ -1,6 +1,7 @@
 classdef Loss < dagnn.ElementWise
   properties
     loss = 'softmaxlog'
+    display = true
     opts = {}
   end
 
@@ -12,6 +13,11 @@ classdef Loss < dagnn.ElementWise
   methods
     function outputs = forward(obj, inputs, params)
       outputs{1} = vl_nnloss(inputs{1}, inputs{2}, [], 'loss', obj.loss, obj.opts{:}) ;
+      obj.account(inputs, outputs);
+    end
+
+    function account(obj, inputs, outputs)
+      if ~obj.display, return; end;
       n = obj.numAveraged ;
       m = n + size(inputs{1},4) ;
       obj.average = (n * obj.average + gather(outputs{1})) / m ;
