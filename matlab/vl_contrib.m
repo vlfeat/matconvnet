b903621455b321a0333fb1009036888d961f339b
Karel Lenc <lenckarel@gmail.com>
2017-04-06 11:46:25 +0100

    Fancyfication of the cl_contrib.
    Supports some new things, like a more detailed list etc.

--- matlab/vl_contrib.m
+++ matlab/vl_contrib.m
@@ -1,6 +1,6 @@
-function vl_contrib(action, module, varargin)
+function res = vl_contrib(command, module, varargin)
 %VL_CONTRIB Contribution modules managemenr
-%  VL_CONTRIB AVAIL
+%  VL_CONTRIB LIST
 %    Prints a list of available modules.
 %
 %  VL_CONTRIB GET MODULE
@@ -20,11 +20,12 @@ opts.contribUrl = 'github.com/lenck/matconvnet-contrib-test/';
 opts.contribDir = fullfile(vl_rootnn(), 'contrib');
 if has_git(), opts.method = 'git'; else, opts.method = 'zip'; end
 opts.force = false;
+opts.showlinks = usejava('desktop');
 opts = vl_argparse(opts, varargin);
-% Support for the old-time calls
+% Support for the script-like calls
 if isstr(opts.force), opts.force = str2num(opts.force); end;
 
-if nargin < 1, action = 'avail'; end;
+if nargin < 1, command = 'list'; end;
 if nargin < 2, module = ''; end;
 
 % Persistent to reduce the reads
@@ -39,42 +40,84 @@ end
 if nargin > 1
   [module_found, module_id] = ismember(module, {contribs.name});
   if ~module_found, error('Unknown module %s.'); end
-  module = contribs(module_id);
+  module = init_module(contribs(module_id), opts);
+end
+
+switch lower(command)
+  case 'list'
+    list_modules(contribs, opts);
+  case {'install', 'update'}
+    module_get(module, opts);
+  case 'compile'
+    module_compile(module);
+  case 'setup'
+    module_setup(module);
+  case 'unload'
+    module_unload(module);
+  case 'url'
+    res = module.repo.url;
+  case 'path'
+    res = module.path;
+  otherwise
+    error('Unknown command %s.', command);
+end
+end
+
+function module = init_module(module, opts)
   module.path = fullfile(opts.contribDir, module.name);
+  module.present = exist(module.path, 'dir');
   module.repo = repo_factory(module.url);
   module.setup_path = fullfile(module.path, ['setup_', module.name, '.m']);
   module.compile_path = fullfile(module.path, ['compile_', module.name, '.m']);
   module.sha_path = fullfile(module.path, '.sha');
 end
 
-% Perform the command
-switch lower(action)
-  case 'avail'
-    avail_modules(contribs);
-  case {'install', 'update'}
-    get_module(module, opts);
-  case 'compile'
-    compile_module(module);
-  case 'setup'
-    setup_module(module);
-  case 'unload'
-    unload_module(module);
-  otherwise
-    error('Unknown action %s.', action);
-end
-
-end
-
-function avail_modules(contribs)
+function list_modules(contribs, opts)
+  function pmodule_links(module)
+    module = init_module(module, opts);
+    fprintf('\t<a href="%s">%s</a>', module.url, module.name);
+    fprintf('\t%s\n\t\t', module.desc);
+    if exist(module.path, 'dir')
+      fprintf('<a href="matlab: vl_contrib update %s">[Update]</a> ', module.name);
+      if exist(module.setup_path, 'file')
+        fprintf('<a href="matlab: vl_contrib setup %s">[Setup]</a> ', module.name);
+      end
+      if exist(module.compile_path, 'file')
+        fprintf('<a href="matlab: vl_contrib compile %s">[Compile]</a> ', module.name);
+      end
+      if module_loaded(module)
+        fprintf('<a href="matlab: vl_contrib unload %s">[Unload]</a> ', module.name);
+      end
+      fprintf('(%s)', get_module_type(module));
+    else
+      fprintf('<a href="matlab: vl_contrib install %s">[Install]</a> ', module.name);
+    end
+    fprintf('\n\n');
+  end
 fprintf('\nAvailable modules:\n\n');
-arrayfun(@(s) fprintf('\t%s\t\t%s\n', s.name, s.desc), contribs);
+if opts.showlinks
+  print_module = @pmodule_links;
+else
+  print_module = @(s) fprintf('\t%s\t\t%s\n', s.name, s.desc);
+end
+arrayfun(@(s) print_module(s), contribs);
+
 fprintf('\n');
 end
 
-function get_module(module, opts)
-switch opts.method
+function module_get(module, opts)
+if exist(module.path, 'dir')
+  method = get_module_type(module);
+  if isempty(method)
+    error('Module %s does not seem to be managed by vl_contrib.', ...
+      module.name);
+  end;
+else
+  method = opts.method;
+end
+switch method
   case 'git'
-    res = get_module_git(module, opts);
+    get_module_git(module, opts);
   case 'zip'
     get_module_zip(module, opts);
   otherwise
@@ -82,19 +125,25 @@ switch opts.method
 end
 end
 
-function setup_module(module)
+function module_setup(module)
 if exist(module.setup_path, 'file')
   run(module.setup_path);
 end
 end
 
-function compile_module(module)
+function module_compile(module)
 if exist(module.compile_path, 'file')
   run(module.compile_path);
 end
 end
 
-function to_unload = unload_module(module)
+function res = module_loaded(module)
+paths = strsplit(path(), ':');
+is_modpath = cellfun(@(s) ~isempty(strfind(s, module.path)), paths);
+res = any(is_modpath);
+end
+
+function to_unload = module_unload(module)
 paths = strsplit(path(), ':');
 is_modpath = cellfun(@(s) ~isempty(strfind(s, module.path)), paths);
 to_unload = any(is_modpath);
@@ -110,17 +159,23 @@ pattern = '\*[\s]*\[(?<name>\w+)\][\s]*\((?<url>.*?)\)[\s]*(?<desc>.*?)\n';
 contribs = regexp(text, pattern, 'lineanchors', 'names');
 end
 
-function res = has_git()
-[ret, ~] = system('which git');
-res = ret == 0;
+function method = get_module_type(module)
+if is_git(module), method = 'git'; return; end;
+if is_zip(module), method = 'zip'; return; end;
+method = '';
+end
+
+function res = verify_user(question)
+answer = input(question, 's');
+res = ~(isempty(answer) || ismember(lower(answer), {'n', 'no'}));
 end
 
 %% GIT Downloader
-function res = get_module_git(module, opts)
+% Basic GIT download
+function get_module_git(module, opts)
   function ret = git(varargin)
     % TODO solve this issue better than just disabling the SSL
-    cmd = {'GIT_SSL_NO_VERIFY=true', 'git'};
-    cmd = [cmd, varargin];
+    cmd = [{'GIT_SSL_NO_VERIFY=true', 'git'}, varargin];
     cmd = strjoin(cmd, ' ');
     [ret, str] = system(cmd);
     if ret ~= 0
@@ -128,33 +183,38 @@ function res = get_module_git(module, opts)
     end
   end
 
-ret = inf;
 if exist(module.path, 'dir')
-  if ~exist(fullfile(module.path, '.git'), 'dir')
+  if ~is_git(module)
     error('Module %s in %s is not a git repo. Change method?', ...
       module.name, module.path)
   end
-  ret = git('--git-dir', fullfile(module.path, '.git'), 'pull');
+  git('--git-dir', fullfile(module.path, '.git'), 'pull');
 else
   fprintf('Initialising git repository %s.\n', module.path);
-  ret = git('clone', module.repo.git_url, module.path);
+  git('clone', module.repo.git_url, module.path);
 end
+end
+
+function res = is_git(module)
+res = exist(fullfile(module.path, '.git'), 'dir');
+end
+
+function res = has_git()
+[ret, ~] = system('which git');
 res = ret == 0;
 end
 
-%% Basic ZIP Downloader
+
+%% ZIP Downloader
 function res = get_module_zip(module, opts)
   function download(module, sha, opts)
     if module.present && ~opts.force
       question = sprintf(...
         'Module `%s` seems to exist. Reinstall? Y/N [N]: ', ...
         module.name);
-      answer = input(question, 's');
-      if isempty(answer) || ismember(lower(answer), {'n', 'no'})
-        return;
-      end
+      if ~verify_user(question), return; end;
     end
-    unloaded = unload_module(module);
+    unloaded = module_unload(module);
     fprintf('Donwloading %s... ', module.name);
     tmp_p = tempname();
     mkdir(tmp_p);
@@ -170,12 +230,11 @@ function res = get_module_zip(module, opts)
     fprintf(fd, sha);
     fclose(fd);
     fprintf('Done\n');
-    if unloaded, setup_module(module); end;
+    if unloaded, module_setup(module); end;
   end
 
 sha = module.repo.get_master_sha();
-present = exist(module.sha_path, 'file');
-if ~present || opts.force
+if ~is_zip(module) || opts.force
   download(module, sha, opts);
   res = true;
   return;
@@ -188,6 +247,10 @@ else
   fprintf('Module `%s` is up to date.\n', module.name);
 end
 res = true;
+end
+
+function res = is_zip(module)
+res = exist(module.sha_path, 'file');
 end
 
 %% Repository wrappers
@@ -209,6 +272,7 @@ function obj = github(url)
 pattern = 'github\.com/(?<user>[\w]*)/(?<repo>[\w-]*)';
 match = regexp(url, pattern, 'names');
 if isempty(match), obj = []; return; end;
+obj.url = url;
 obj.git_url = [url, '.git'];
 obj.readme_url = ...
   sprintf('https://raw.githubusercontent.com/%s/%s/master/README.md', ...
