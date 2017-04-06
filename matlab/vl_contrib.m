5abe6c6794a3c954def85d2c141a1268eae122a4
Karel Lenc <lenckarel@gmail.com>
2017-04-06 17:13:18 +0100

    Beautification of the code.

--- matlab/vl_contrib.m
+++ matlab/vl_contrib.m
@@ -3,25 +3,34 @@ function res = vl_contrib(command, module, varargin)
 %  VL_CONTRIB LIST
 %    Prints a list of available modules.
 %
-%  VL_CONTRIB GET MODULE
+%  VL_CONTRIB INSTALL MODULE
 %    Downloads a module MODULE.
 %
 %  VL_CONTRIB SETUP MODULE
-%    Setups a module MODULE.
+%    Setups paths of a MODULE.
 %
-%   See also: VL_NNSETUP, VL_NNCOMPILE, VL_TESTNN.
+%  VL_CONTRIB SETUP MODULE
+%    Setups paths of a MODULE.
+%
+%  VL_CONTRIB COMPILE MODULE ...
+%    Compile a MODULE.
+%
+%   See also: VL_SETUPNN, VL_COMPILENN, VL_TESTNN.
 
-% Copyright (C) 2017 Karel Lenc.
+% Copyright (C) 2017 Karel Lenc and Joao Henriques.
 % All rights reserved.
 %
 % This file is part of the VLFeat library and is made available under
 % the terms of the BSD license (see the COPYING file).
+root = fileparts(fileparts(mfilename('fullpath'))) ;
+addpath(fullfile(root, 'matlab')) ;
+
 opts.contribUrl = 'github.com/lenck/matconvnet-contrib-test/';
 opts.contribDir = fullfile(vl_rootnn(), 'contrib');
 if has_git(), opts.method = 'git'; else, opts.method = 'zip'; end
 opts.force = false;
 opts.showlinks = usejava('desktop');
-opts = vl_argparse(opts, varargin);
+[opts, varargin] = vl_argparse(opts, varargin);
 % Support for the script-like calls
 if isstr(opts.force), opts.force = str2num(opts.force); end;
 
@@ -40,16 +49,18 @@ end
 if nargin > 1
   [module_found, module_id] = ismember(module, {contribs.name});
   if ~module_found, error('Unknown module %s.'); end
-  module = init_module(contribs(module_id), opts);
+  module = module_init(contribs(module_id), opts);
 end
 
 switch lower(command)
   case 'list'
-    list_modules(contribs, opts);
+    fprintf('\nAvailable modules:\n\n');
+    arrayfun(@(s) module_print(s, opts), contribs);
+    fprintf('\n');
   case {'install', 'update'}
     module_get(module, opts);
   case 'compile'
-    module_compile(module);
+    module_compile(module, varargin);
   case 'setup'
     module_setup(module);
   case 'unload'
@@ -63,49 +74,62 @@ switch lower(command)
 end
 end
 
-function module = init_module(module, opts)
-  module.path = fullfile(opts.contribDir, module.name);
-  module.present = exist(module.path, 'dir');
-  module.repo = repo_factory(module.url);
-  module.setup_path = fullfile(module.path, ['setup_', module.name, '.m']);
-  module.compile_path = fullfile(module.path, ['compile_', module.name, '.m']);
-  module.sha_path = fullfile(module.path, '.sha');
+% --------------------------------------------------------------------
+%                                                     Module functions
+% --------------------------------------------------------------------
+% --------------------------------------------------------------------
+function module = module_init(module, opts)
+% --------------------------------------------------------------------
+name2path = @(name) strrep(name, '-', '_');
+module.path = fullfile(opts.contribDir, module.name);
+module.present = exist(module.path, 'dir');
+module.repo = repo_factory(module.url);
+module.setup_path = fullfile(module.path, ...
+  ['setup_', name2path(module.name), '.m']);
+module.compile_path = fullfile(module.path, ...
+  ['compile_', name2path(module.name), '.m']);
+module.sha_path = fullfile(module.path, '.sha');
 end
 
-function list_modules(contribs, opts)
-  function pmodule_links(module)
-    module = init_module(module, opts);
-    fprintf('\t<a href="%s">%s</a>', module.url, module.name);
-    fprintf('\t%s\n\t\t', module.desc);
-    if exist(module.path, 'dir')
-      fprintf('<a href="matlab: vl_contrib update %s">[Update]</a> ', module.name);
-      if module_loaded(module)
-        fprintf('<a href="matlab: vl_contrib unload %s">[Unload]</a> ', module.name);
-      elseif exist(module.setup_path, 'file')
-        fprintf('<a href="matlab: vl_contrib setup %s">[Setup]</a> ', module.name);
-      end
-      if exist(module.compile_path, 'file')
-        fprintf('<a href="matlab: vl_contrib compile %s">[Compile]</a> ', module.name);
-      end
-
-      fprintf('(%s)', get_module_type(module));
-    else
-      fprintf('<a href="matlab: vl_contrib install %s">[Install]</a> ', module.name);
-    end
-    fprintf('\n\n');
-  end
-fprintf('\nAvailable modules:\n\n');
+% --------------------------------------------------------------------
+function module_print(module, opts)
+% --------------------------------------------------------------------
+module = module_init(module, opts);
 if opts.showlinks
-  print_module = @pmodule_links;
+  fprintf('\t<a href="%s">%s</a>', module.url, module.name);
+  fprintf('\t%s\n\t\t', module.desc);
+  if exist(module.path, 'dir')
+    fprintf('<a href="matlab: vl_contrib update %s">[Update]</a> ', ...
+      module.name);
+    if module_loaded(module)
+      fprintf('<a href="matlab: vl_contrib unload %s">[Unload]</a> ', ...
+        module.name);
+    elseif exist(module.setup_path, 'file')
+      fprintf('<a href="matlab: vl_contrib setup %s">[Setup]</a> ', ...
+        module.name);
+    end
+    if exist(module.compile_path, 'file')
+      fprintf('<a href="matlab: vl_contrib compile %s">[Compile]</a> ', ...
+        module.name);
+    end
+    fprintf('(%s)', get_module_type(module));
+  else
+    fprintf('<a href="matlab: vl_contrib install %s">[Install]</a> ', ...
+      module.name);
+  end
+  fprintf('\n\n');
 else
-  print_module = @(s) fprintf('\t%s\t\t%s\n', s.name, s.desc);
+  fprintf('\t%s\t\t%s', module.name, module.desc);
+  if exist(module.path, 'dir')
+    fprintf(' (%s)', get_module_type(module));
+  end
+  fprintf('\n');
 end
-arrayfun(@(s) print_module(s), contribs);
-
-fprintf('\n');
 end
 
+% --------------------------------------------------------------------
 function module_get(module, opts)
+% --------------------------------------------------------------------
 if exist(module.path, 'dir')
   method = get_module_type(module);
   if isempty(method)
@@ -117,33 +141,44 @@ else
 end
 switch method
   case 'git'
-    get_module_git(module, opts);
+    git_get_module(module, opts);
   case 'zip'
-    get_module_zip(module, opts);
+    zip_get_module(module, opts);
   otherwise
     error('Unknown method %s.', opts.method);
 end
 end
 
+% --------------------------------------------------------------------
 function module_setup(module)
+% --------------------------------------------------------------------
 if exist(module.setup_path, 'file')
   run(module.setup_path);
 end
 end
 
-function module_compile(module)
+% --------------------------------------------------------------------
+function module_compile(module, varargin)
+% --------------------------------------------------------------------
 if exist(module.compile_path, 'file')
-  run(module.compile_path);
+  module_setup(module);
+  [~, compile_nm, ~] = fileparts(module.compile_path);
+  handle = str2func(compile_nm);
+  handle(varargin{:});
 end
 end
 
+% --------------------------------------------------------------------
 function res = module_loaded(module)
+% --------------------------------------------------------------------
 paths = strsplit(path(), ':');
 is_modpath = cellfun(@(s) ~isempty(strfind(s, module.path)), paths);
 res = any(is_modpath);
 end
 
+% --------------------------------------------------------------------
 function to_unload = module_unload(module)
+% --------------------------------------------------------------------
 paths = strsplit(path(), ':');
 is_modpath = cellfun(@(s) ~isempty(strfind(s, module.path)), paths);
 to_unload = any(is_modpath);
@@ -153,26 +188,19 @@ if to_unload
 end
 end
 
-%% Parse contribution files
-function contribs = parse_contributions(text)
-pattern = '\*[\s]*\[(?<name>\w+)\][\s]*\((?<url>.*?)\)[\s]*(?<desc>.*?)\n';
-contribs = regexp(text, pattern, 'lineanchors', 'names');
+% --------------------------------------------------------------------
+%                                                   Download functions
+% --------------------------------------------------------------------
+
+% --------------------------------------------------------------------
+function res = is_git(module)
+% --------------------------------------------------------------------
+res = exist(fullfile(module.path, '.git'), 'dir');
 end
 
-function method = get_module_type(module)
-if is_git(module), method = 'git'; return; end;
-if is_zip(module), method = 'zip'; return; end;
-method = '';
-end
-
-function res = verify_user(question)
-answer = input(question, 's');
-res = ~(isempty(answer) || ismember(lower(answer), {'n', 'no'}));
-end
-
-%% GIT Downloader
-% Basic GIT download
-function get_module_git(module, opts)
+% --------------------------------------------------------------------
+function git_get_module(module, opts)
+% --------------------------------------------------------------------
   function ret = git(varargin)
     % TODO solve this issue better than just disabling the SSL
     cmd = [{'GIT_SSL_NO_VERIFY=true', 'git'}, varargin];
@@ -195,18 +223,15 @@ else
 end
 end
 
-function res = is_git(module)
-res = exist(fullfile(module.path, '.git'), 'dir');
+% --------------------------------------------------------------------
+function res = is_zip(module)
+% --------------------------------------------------------------------
+res = exist(module.sha_path, 'file');
 end
 
-function res = has_git()
-[ret, ~] = system('which git');
-res = ret == 0;
-end
-
-
-%% ZIP Downloader
-function res = get_module_zip(module, opts)
+% --------------------------------------------------------------------
+function res = zip_get_module(module, opts)
+% --------------------------------------------------------------------
   function download(module, sha, opts)
     if module.present && ~opts.force
       question = sprintf(...
@@ -249,18 +274,21 @@ end
 res = true;
 end
 
-function res = is_zip(module)
-res = exist(module.sha_path, 'file');
-end
+% --------------------------------------------------------------------
+%                                                  Repository wrappers
+% --------------------------------------------------------------------
 
-%% Repository wrappers
+% --------------------------------------------------------------------
 function repo = repo_factory(url)
+% --------------------------------------------------------------------
 repo = github(url);
 if ~isempty(repo), return; end;
 error('Invalid repository %s.', url);
 end
 
+% --------------------------------------------------------------------
 function obj = github(url)
+% --------------------------------------------------------------------
   function sha = read_sha(match)
     master_sha_url = ...
       sprintf('https://api.github.com/repos/%s/%s/git/refs/heads/master',...
@@ -284,3 +312,36 @@ obj.get_master_sha = @() read_sha(match);
 end
 
 % TODO implemented BitBucket wrapper
+
+% --------------------------------------------------------------------
+%                                                    Utility functions
+% --------------------------------------------------------------------
+
+% --------------------------------------------------------------------
+function res = has_git()
+% --------------------------------------------------------------------
+[ret, ~] = system('which git');
+res = ret == 0;
+end
+
+% --------------------------------------------------------------------
+function contribs = parse_contributions(text)
+% --------------------------------------------------------------------
+pattern = '\*[\s]*\[(?<name>\w+)\][\s]*\((?<url>.*?)\)[\s]*(?<desc>.*?)\n';
+contribs = regexp(text, pattern, 'lineanchors', 'names');
+end
+
+% --------------------------------------------------------------------
+function method = get_module_type(module)
+% --------------------------------------------------------------------
+if is_git(module), method = 'git'; return; end;
+if is_zip(module), method = 'zip'; return; end;
+method = '';
+end
+
+% --------------------------------------------------------------------
+function res = verify_user(question)
+% --------------------------------------------------------------------
+answer = input(question, 's');
+res = ~(isempty(answer) || ismember(lower(answer), {'n', 'no'}));
+end
