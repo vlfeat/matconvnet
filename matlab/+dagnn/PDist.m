1f8fac16d756cc8af0be3620d31891bced063e4c
Karel Lenc <lenckarel@gmail.com>
2017-09-20 15:17:08 +0100

    Added more options for the dagnn.PDist wrapper.

--- matlab/+dagnn/PDist.m
+++ matlab/+dagnn/PDist.m
@@ -7,18 +7,21 @@ classdef PDist < dagnn.Loss
   properties
     p = 2;
     aggregate = true;
+    noRoot = false;
+    epsilon = 1e-6;
   end
 
   methods
     function outputs = forward(obj, inputs, params)
+      args = {'aggregate', obj.aggregate, 'noRoot', obj.noRoot, ...
+          'epsilon', obj.epsilon};
       switch numel(inputs)
         case 2
           outputs{1} = vl_nnpdist(inputs{1}, inputs{2}, obj.p, [], ...
-            'aggregate', obj.aggregate, obj.opts{:}) ;
+              args{:}, obj.opts{:}) ;
         case 3
           outputs{1} = vl_nnpdist(inputs{1}, inputs{2}, obj.p, [], ...
-            'aggregate', obj.aggregate, 'instanceWeights', inputs{3}, ...
-            obj.opts{:}) ;
+            args{:}, 'instanceWeights', inputs{3}, obj.opts{:}) ;
         otherwise
           error('Invalid number of inputs');
       end
@@ -27,14 +30,16 @@ classdef PDist < dagnn.Loss
 
     function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
       derInputs = cell(1, numel(inputs));
+      args = {'aggregate', obj.aggregate, 'noRoot', obj.noRoot, ...
+          'epsilon', obj.epsilon};
       switch numel(inputs)
         case 2
           [derInputs{1}, derInputs{2}] = vl_nnpdist(inputs{1}, inputs{2}, ...
-            obj.p, derOutputs{1}, 'aggregate', obj.aggregate, obj.opts{:}) ;
+            obj.p, derOutputs{1}, args{:}, obj.opts{:}) ;
         case 3
           [derInputs{1}, derInputs{2}] = vl_nnpdist(inputs{1}, inputs{2}, ...
-            obj.p, derOutputs{1}, 'aggregate', obj.aggregate, ...
-            'instanceWeights', inputs{3}, obj.opts{:}) ;
+            obj.p, derOutputs{1}, args{:}, 'instanceWeights', inputs{3}, ...
+            obj.opts{:}) ;
         otherwise
           error('Invalid number of inputs');
       end
